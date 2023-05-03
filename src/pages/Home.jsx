3136904616ee15e73990eeedbff13a93a68540f3
Kamzen <kamzen1994@gmail.com>
2023-05-03 19:26:35 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,29 @@
-import { Stack } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 import React from "react";
 import WelcomeComponent from "../components/WelcomeComponent";
 import Testimony from "../components/Testimony";
 import ContactFooter from "../components/ContactFooter";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const Home = () => {
+const Home = ({ id }) => {
+  const [contactRef, setContactRef] = useState();
+  useEffect(() => {
+    if (id && contactRef) {
+      window.scrollTo({
+        top: contactRef.current.offsetTop,
+        behavior: "smooth"
+      });
+    }
+  }, [contactRef, id]);
+
   return (
     <Stack spacing={2}>
       <WelcomeComponent />
       <Stack paddingX={10} spacing={2}>
         <Testimony />
-        <ContactFooter />
+
+        <ContactFooter setContactRef={setContactRef} />
       </Stack>
     </Stack>
   );

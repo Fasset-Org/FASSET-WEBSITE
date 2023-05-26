27507c90dffacf791c8b4c379d03e34ae77818b3
Kamzen <kamzen1994@gmail.com>
2023-05-26 09:15:10 +0200

    Create ExecutiveCard.jsx

---
+++ src/components/AboutUs/ExecutiveCard.jsx
@@ -0,0 +1,12 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+const ExecutiveCard = () => {
+  return (
+    <Grid item xs={12} md={4} padding={2} border={1}>
+
+    </Grid>
+  )
+}
+
+export default ExecutiveCard

b9c3cc5bafb2da99a14efb65195cb9c40c31f32a
Kamzen <kamzen1994@gmail.com>
2023-05-26 09:14:56 +0200

    Create LatestNews.jsx

---
+++ src/components/Latest News/LatestNews.jsx
@@ -0,0 +1,12 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+
+const LatestNews = () => {
+  return (
+    <Stack height={300} border={1} borderColor='lightgray'>
+
+    </Stack>
+  )
+}
+
+export default LatestNews

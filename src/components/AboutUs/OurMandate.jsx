9c25ba87a9f03b8c74736f60e6b760e75c030ade
Kamzen <kamzen1994@gmail.com>
2023-05-26 09:14:59 +0200

    Update OurMandate.jsx

--- src/components/AboutUs/OurMandate.jsx
+++ src/components/AboutUs/OurMandate.jsx
@@ -1,75 +1,96 @@
 import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
   Box,
+  Collapse,
+  Grid,
+  IconButton,
   List,
   ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
   Stack,
   Typography
 } from "@mui/material";
 import React from "react";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { StarBorder } from "@mui/icons-material";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
 
 const OurMandate = () => {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
   return (
-    <Box sx={{ backgroundColor: "#f2f3f3" }} padding={2}>
-      <Stack
-        direction="row"
-        mb={2}
-        width="100%"
-        justifyContent="center"
-        alignContent="center"
-      >
-        <Typography
-          fontSize={30}
-          fontWeight="bolder"
-          textTransform="uppercase"
-          fontFamily="Helvetica Neue"
-          textAlign="center"
-          sx={{ color: "primary.main" }}
-        >
-          Our Mandate
-        </Typography>
-      </Stack>
-      <Stack direction="row" spacing={2}>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon sx={{ color: "#fff200" }} />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            sx={{ backgroundColor: "primary.main" }}
+    <Box>
+      <Stack spacing={2}>
+        <Stack justifyContent="center" alignItems="center">
+          <IconButton size="large">
+            <SettingsIcon fontSize="large" sx={{ color: "secondary.main" }} />
+          </IconButton>
+          <Typography
+            textAlign="center"
+            fontFamily="Helvetica Neue"
+            fontWeight="bolder"
+            fontSize={30}
+            textTransform="uppercase"
+            sx={{ color: "primary.main" }}
           >
-            <Typography fontSize={15}>Competencies Required</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
+            Our Mandate
+          </Typography>
+        </Stack>
+        <Grid container>
+          <Grid item xs={12} md={4}>
             <List
-              sx={{
-                width: "100%"
-                // bgcolor: "background.paper"
-              }}
+              sx={{ width: "100%", bgcolor: "background.paper", border: 1 }}
+              component="nav"
+              aria-labelledby="nested-list-subheader"
+              subheader={
+                <ListSubheader
+                  component="div"
+                  id="nested-list-subheader"
+                  sx={{
+                    fontWeight: "bolder",
+                    color: "primary.main",
+                    border: 1
+                  }}
+                >
+                  The Objectives of Fasset are the following:
+                </ListSubheader>
+              }
             >
-              {[
-                "Solid computer skills: MS Office Suite",
-                "Excellent command of English (verbal and written).",
-                "Project planning and administration ability.",
-                "Ability to work under pressure and meet deadlines.",
-                "Good relationship with stakeholders and an energetic approach to work.",
-                "Detail and process orientated.",
-                "Strong analytical, interpersonal, communication.",
-                "Report writing and presentation skills",
-                "Ability to work independently and as part of a team"
-              ].map((responsibility, i) => (
-                <ListItem key={i} disableGutters>
-                  <Typography fontWeight="bolder" sx={{ mr: 2 }}>
-                    {i + 1}.
-                  </Typography>
-                  <Typography>{responsibility}</Typography>
-                </ListItem>
-              ))}
+              <ListItem sx={{ border: 1 }}>
+                <Typography
+                  sx={{
+                    fontSize: 14
+                    // textAlign: "center"
+                    // backgroundColor: idx % 2 === 1 ? "#E5E7E9" : ""
+                  }}
+                >{`• ${"Item 1"}`}</Typography>
+              </ListItem>
+              <ListItemButton>
+                <ListItemText primary="Drafts" />
+              </ListItemButton>
+              <ListItemButton onClick={handleClick}>
+                <ListItemText primary="Inbox" />
+                {open ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <StarBorder />
+                    </ListItemIcon>
+                    <ListItemText primary="Starred" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
             </List>
-          </AccordionDetails>
-        </Accordion>
+          </Grid>
+        </Grid>
       </Stack>
     </Box>
   );

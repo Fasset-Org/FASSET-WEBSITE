7ae7e44f1e949c1acbdd7ac01b3ff3a08b140bba
Kamzen <kamzen1994@gmail.com>
2023-05-26 09:14:11 +0200

    Update AboutUs.jsx

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import WhoWeAre from "../../components/WhoWeAre";
-import { Box } from "@mui/material";
+import { Stack } from "@mui/material";
+import WelcomeComponent from "../../components/WelcomeComponent";
+import AboutBanner from "../../components/AboutUs/AboutBanner";
+import AboutInfo from "../../components/AboutUs/AboutInfo";
 import OurMandate from "../../components/AboutUs/OurMandate";
+import Executives from "../../components/AboutUs/Executives";
 
 const AboutUs = () => {
   return (
-    <Box>
-      <WhoWeAre />
-      <OurMandate />
-    </Box>
+    <Stack spacing={2}>
+      {/* <WhoWeAre />
+      <OurMandate /> */}
+      <AboutBanner />
+      <Stack px={15} spacing={2}>
+        <WelcomeComponent />
+        <AboutInfo />
+        <OurMandate />
+        <Executives />
+      </Stack>
+    </Stack>
   );
 };
 

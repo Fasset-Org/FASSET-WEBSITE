edec3ab32923363ade4567c2a5b68c8df1341069
Kamzen <kamzen1994@gmail.com>
2023-05-26 09:14:07 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,24 @@
 import React from "react";
-// import WelcomeComponent from "../components/WelcomeComponent";
-
-// import ContactFooter from "../components/ContactFooter";
-// import { useState } from "react";
-// import { useEffect } from "react";
+import WelcomeComponent from "../components/WelcomeComponent";
 import { Stack } from "@mui/material";
-import DesktopBanner from "../components/Desktop/DesktopBanner";
-import LinksFooter from "../components/LinksFooter";
+import LatestNews from "../components/Latest News/LatestNews";
+import banner1 from "../images/bgImage1.avif";
+import banner2 from "../images/Website-Deadline-Extension.png";
+import banner3 from "../images/sigmund-LCun3uxh-z0-unsplash.jpg";
+import banner4 from "../images/gallery-images/Nkandla.jpg";
+import Slideshow from "../components/SlideShow";
 
 const Home = ({ id }) => {
-  // const [contactRef, setContactRef] = useState();
-  // useEffect(() => {
-  //   if (id && contactRef) {
-  //     window.scrollTo({
-  //       top: contactRef.current.offsetTop,
-  //       behavior: "smooth"
-  //     });
-  //   }
-  // }, [contactRef, id]);
+  const images2 = [banner1, banner2, banner3, banner4];
 
   return (
-    <Stack spacing={2}>
-      <DesktopBanner />
-
-      <LinksFooter />
+    <Stack spacing={2} width="100%">
+      {/* <DesktopBanner /> */}
+      <Slideshow images={images2} height="500px" />
+      <Stack paddingX={15}>
+        <WelcomeComponent />
+      </Stack>
+      <LatestNews />
     </Stack>
   );
 };

1ce9b4e9f32aa321b78b03218454831c72b0685e
Kamzen <kamzen1994@gmail.com>
2023-05-31 11:15:07 +0200

    Create UsersList.jsx

---
+++ src/pages/FMS/Auth/User/UsersList.jsx
@@ -0,0 +1,12 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+
+const UsersList = () => {
+  return (
+    <Stack>
+      UsersList
+    </Stack>
+  )
+}
+
+export default UsersList

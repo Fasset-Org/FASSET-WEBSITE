f6a7d7a1c40579f3ef282900f5639e0bf6084cd4
Kamzen <kamzen1994@gmail.com>
2023-05-31 11:14:56 +0200

    Update Dashboard.jsx

--- src/pages/FMS/Auth/Dashboard.jsx
+++ src/pages/FMS/Auth/Dashboard.jsx
@@ -4,43 +4,26 @@ import DashboardCard from "../../../components/FMS/DashboardCard";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 import DevicesOtherIcon from "@mui/icons-material/DevicesOther";
 import WorkIcon from "@mui/icons-material/Work";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
   return (
     <Stack>
       <Grid container spacing={6}>
         <Grid item md={3}>
-          <DashboardCard title="User Management" Icon={ManageAccountsIcon} />
+          <DashboardCard
+            title="User Management"
+            Icon={ManageAccountsIcon}
+            onClick={() => navigate("/fms/users")}
+          />
         </Grid>
         <Grid item md={3}>
-          <DashboardCard title="Asset Management" Icon={DevicesOtherIcon} />
-        </Grid>
-        <Grid item md={3}>
-          <DashboardCard title="Human Resource" Icon={WorkIcon} />
-        </Grid>
-        <Grid item md={3}>
-          <DashboardCard title="Human Resource" Icon={WorkIcon} />
-        </Grid>
-        <Grid item md={3}>
-          <DashboardCard title="User Management" Icon={ManageAccountsIcon} />
-        </Grid>
-        <Grid item md={3}>
-          <DashboardCard title="Asset Management" Icon={DevicesOtherIcon} />
-        </Grid>
-        <Grid item md={3}>
-          <DashboardCard title="Human Resource" Icon={WorkIcon} />
-        </Grid>
-        <Grid item md={3}>
-          <DashboardCard title="Human Resource" Icon={WorkIcon} />
-        </Grid>
-        <Grid item md={3}>
-          <DashboardCard title="User Management" Icon={ManageAccountsIcon} />
-        </Grid>
-        <Grid item md={3}>
-          <DashboardCard title="Asset Management" Icon={DevicesOtherIcon} />
-        </Grid>
-        <Grid item md={3}>
-          <DashboardCard title="Human Resource" Icon={WorkIcon} />
+          <DashboardCard
+            title="Asset Management"
+            Icon={DevicesOtherIcon}
+            onClick={() => navigate("/fms/assets")}
+          />
         </Grid>
         <Grid item md={3}>
           <DashboardCard title="Human Resource" Icon={WorkIcon} />

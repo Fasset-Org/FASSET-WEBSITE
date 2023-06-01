b352b01fa9665c938ba8778f18ef202578ddf059
Kamzen <kamzen1994@gmail.com>
2023-06-01 13:20:28 +0200

    Create TopLinksNavigation.jsx

---
+++ src/components/Navigation/TopLinksNavigation.jsx
@@ -0,0 +1,23 @@
+import { Link, Stack } from "@mui/material";
+import React from "react";
+import logo from '../../images/fasset_master_logo.png';
+
+const TopLinksNavigation = () => {
+  return (
+    <Stack
+      height={50}
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      spacing={4}
+    >
+      <Link sx={{ color: "black" }}>INVOICE MANAGEMENT</Link>
+      <Link sx={{ color: "black" }}>ODOO LMIS</Link>
+      <img src={logo} alt="" height={30} width={180} />
+      <Link sx={{ color: "black" }}>BURSARY MANGEMENT SYSTEM</Link>
+      <Link sx={{ color: "black" }}>CAREER PORTAL</Link>
+    </Stack>
+  );
+};
+
+export default TopLinksNavigation;

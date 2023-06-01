3b084c406f4fa0902e5a079922aca2a6c6d74bdf
Kamzen <kamzen1994@gmail.com>
2023-06-01 13:20:25 +0200

    Create TopContactNavigation.jsx

---
+++ src/components/Navigation/TopContactNavigation.jsx
@@ -0,0 +1,79 @@
+import { Divider, Stack, Typography } from "@mui/material";
+import React from "react";
+import PhoneIcon from "@mui/icons-material/Phone";
+import ContactPhoneIcon from "@mui/icons-material/ContactPhone";
+import MailIcon from "@mui/icons-material/Mail";
+
+const TopContactNavigation = () => {
+  return (
+    <Stack height={40} direction="row" justifyContent="center">
+      <Stack
+        height="100%"
+        width="80%"
+        sx={{
+          backgroundColor: "primary.main",
+          color: "#FFFFFF"
+        }}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={4}
+      >
+        {/* Call Center */}
+        <Typography
+          component={Stack}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={1}
+        >
+          <ContactPhoneIcon sx={{ fontWeight: "bolder" }} />
+
+          <Typography fontWeight="bolder">Call Centre :</Typography>
+          <Typography>&nbsp;087 821 2680</Typography>
+        </Typography>
+        <Divider
+          orientation="vertical"
+          sx={{ backgroundColor: "#FFFFFF", height: "60%" }}
+        />
+
+        {/* SwitchBoard */}
+
+        <Typography
+          component={Stack}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={1}
+        >
+          <PhoneIcon sx={{ fontWeight: "bolder" }} />
+
+          <Typography fontWeight="bolder">Switchboard :</Typography>
+          <Typography>&nbsp;087 821 2680</Typography>
+        </Typography>
+        <Divider
+          orientation="vertical"
+          sx={{ backgroundColor: "#FFFFFF", height: "60%" }}
+        />
+
+        {/* Call Centre Email */}
+        <Typography
+          component={Stack}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={1}
+        >
+          <MailIcon sx={{ fontWeight: "bolder" }} />
+
+          <Typography fontWeight="bolder">Email :</Typography>
+          <Typography>
+            &nbsp;fassetcallcentre@fasset.org.za
+          </Typography>
+        </Typography>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default TopContactNavigation;

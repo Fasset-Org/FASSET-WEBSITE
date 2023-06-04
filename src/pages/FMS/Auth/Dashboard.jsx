4c07db2f81b32c5e2cef5e07ab400b4ba0a52bb4
Kamzen <kamzen1994@gmail.com>
2023-06-04 15:18:11 +0200

    Update Dashboard.jsx

--- src/pages/FMS/Auth/Dashboard.jsx
+++ src/pages/FMS/Auth/Dashboard.jsx
@@ -4,28 +4,35 @@ import DashboardCard from "../../../components/FMS/DashboardCard";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 import DevicesOtherIcon from "@mui/icons-material/DevicesOther";
 import WorkIcon from "@mui/icons-material/Work";
+import LanguageIcon from "@mui/icons-material/Language";
 import { useNavigate } from "react-router-dom";
+import { QueryClient } from "@tanstack/react-query";
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const queryClient = new QueryClient();
+  console.log(queryClient);
   return (
     <Stack>
       <Grid container spacing={6}>
-        <Grid item md={3}>
+        <Grid item md={3} xs={12}>
+          <DashboardCard title="Website Management" Icon={LanguageIcon} />
+        </Grid>
+        <Grid item md={3} xs={12}>
           <DashboardCard
             title="User Management"
             Icon={ManageAccountsIcon}
             onClick={() => navigate("/fms/users")}
           />
         </Grid>
-        <Grid item md={3}>
+        <Grid item md={3} xs={12}>
           <DashboardCard
             title="Asset Management"
             Icon={DevicesOtherIcon}
             onClick={() => navigate("/fms/assets")}
           />
         </Grid>
-        <Grid item md={3}>
+        <Grid item md={3} xs={12}>
           <DashboardCard title="Human Resource" Icon={WorkIcon} />
         </Grid>
       </Grid>

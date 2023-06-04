0b8b439dc562348429ddd04430083437eb4fe437
Kamzen <kamzen1994@gmail.com>
2023-06-04 15:17:54 +0200

    Update DashboardCard.jsx

--- src/components/FMS/DashboardCard.jsx
+++ src/components/FMS/DashboardCard.jsx
@@ -1,10 +1,4 @@
-import {
-  Card,
-  CardContent,
-  IconButton,
-  Stack,
-  Typography
-} from "@mui/material";
+import { Card, CardContent, Stack, Typography } from "@mui/material";
 import React from "react";
 
 const DashboardCard = ({ title, Icon, ...otherProps }) => {
@@ -17,17 +11,16 @@ const DashboardCard = ({ title, Icon, ...otherProps }) => {
       sx={{
         cursor: "pointer",
         border: 2,
-        borderColor: "primary.main",
+        borderColor: "#5D6D7E",
         // backgroundColor: "#374370",
         borderRadius: 4
       }}
       {...otherProps}
+      className="linear-bg-2"
     >
-      <CardContent component={Stack}>
-        <IconButton>
-          <Icon fontSize="large" sx={{ color: "#000000" }} />
-        </IconButton>
-        <Typography sx={{ color: "#000000" }} fontSize={17} fontWeight="bolder">
+      <CardContent component={Stack} alignItems="center">
+        <Icon fontSize="large" sx={{ color: "#FFFFFF" }} />
+        <Typography sx={{ color: "#FFFFFF" }} fontSize={17} fontWeight="bolder">
           {title}
         </Typography>
       </CardContent>

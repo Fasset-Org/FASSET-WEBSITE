38734dbdb95c61dbb57351121bf1dd58abc955eb
Kamzen <kamzen1994@gmail.com>
2023-06-07 09:31:15 +0200

    Update Learners.jsx

--- src/pages/Learners/Learners.jsx
+++ src/pages/Learners/Learners.jsx
@@ -1,54 +1,124 @@
-import { Box, Stack } from "@mui/material";
 import React from "react";
-import "./Learners.scss";
-import learnerImageBanner from "../../images/learner-banner.jpg";
-import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`
+  };
+}
 
 const Learners = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <Box>
-      <Stack
-        height={400}
-        width="100%"
-        border={1}
-        className="linear-bg"
-        direction="row"
-        sx={
-          {
-            // backgroundImage: `url(${learnerImageBanner})`,
-            // backgroundRepeat: "no-repeat",
-            // objectFit: 'scale-down',
-            // backgroundPosition: 'right'
-          }
-        }
+    <Box
+      sx={{
+        flexGrow: 1,
+        bgcolor: "background.paper",
+        display: "flex",
+        height: 400
+      }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: "divider", width: 200 }}
       >
-        <Stack
-          flex={1}
-          height="100%"
-          border={1}
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Link style={{ color: "white" }}>Learnerships</Link>
-          <Link style={{ color: "white" }}>Learnerships</Link>
-          <Link style={{ color: "white" }}>Learnerships</Link>
-          <Link style={{ color: "white" }}>Learnerships</Link>
-          <Link style={{ color: "white" }}>Learnerships</Link>
-        </Stack>
-        <Stack
-          flex={3}
-          height="100%"
-          border={1}
-          sx={
-            {
-              backgroundImage: `url(${learnerImageBanner})`,
-              backgroundRepeat: "no-repeat",
-              objectFit: 'scale-down',
-              backgroundPosition: 'right'
-            }
-          }
-        ></Stack>
-      </Stack>
+        <Tab label="Item One" {...a11yProps(0)} />
+        <Tab label="Item Two" {...a11yProps(1)} />
+        <Tab label="Item Three" {...a11yProps(2)} />
+        <Tab label="Item Four" {...a11yProps(3)} />
+        <Tab label="Item Five" {...a11yProps(4)} />
+        <Tab label="Item Six" {...a11yProps(5)} />
+        <Tab label="Item Seven" {...a11yProps(6)} />
+        <Tab label="Item Eight" {...a11yProps(7)} />
+        <Tab label="Item Nine" {...a11yProps(8)} />
+        <Tab label="Item Ten" {...a11yProps(9)} />
+        <Tab label="Item Eleven" {...a11yProps(10)} />
+        <Tab label="Item Twelve" {...a11yProps(11)} />
+        <Tab label="Item Thirteen" {...a11yProps(12)} />
+        <Tab label="Item Fourteen" {...a11yProps(13)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Five
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        Item Six
+      </TabPanel>
+      <TabPanel value={value} index={6}>
+        Item Seven
+      </TabPanel>
+      <TabPanel value={value} index={7}>
+        Item Eight
+      </TabPanel>
+      <TabPanel value={value} index={8}>
+        Item Nine
+      </TabPanel>
+      <TabPanel value={value} index={9}>
+        Item Ten
+      </TabPanel>
+      <TabPanel value={value} index={10}>
+        Item Eleven
+      </TabPanel>
+      <TabPanel value={value} index={11}>
+        Item Twelve
+      </TabPanel>
+      <TabPanel value={value} index={12}>
+        Item Thirteen
+      </TabPanel>
+      <TabPanel value={value} index={13}>
+        Item Fourteen
+      </TabPanel>
     </Box>
   );
 };

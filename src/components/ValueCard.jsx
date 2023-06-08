890139d268bd4ca675b85dd2134e99329c3a6255
Kamzen <kamzen1994@gmail.com>
2023-06-08 15:54:31 +0200

    Update ValueCard.jsx

--- src/components/ValueCard.jsx
+++ src/components/ValueCard.jsx
@@ -3,44 +3,42 @@ import React from "react";
 
 const ValueCard = ({ title, definition }) => {
   return (
-    <Stack
-      height={250}
-      width="20%"
-      component={Card}
-      sx={{ position: "relative", "&:hover": { scale: 1.2 } }}
-    >
-      <Stack
-        height={210}
-        alignItems="center"
-        justifyContent="center"
-        direction="row"
-        position="absolute"
-        top={0}
-        padding={2}
-      >
-        <Typography textAlign="center">{definition}</Typography>
+    <Card className="v-card" sx={{ height: 250, width: "20%" }}>
+      <Stack height='100%' sx={{ position: "relative" }}>
+        <Stack
+          height={210}
+          alignItems="center"
+          justifyContent="center"
+          direction="row"
+          position="absolute"
+          top={0}
+          padding={2}
+        >
+          <Typography textAlign="center">{definition}</Typography>
+        </Stack>
+        <Box
+          border={1}
+          height={40}
+          width="100%"
+          textAlign="center"
+          component={Stack}
+          alignItems="center"
+          justifyContent="center"
+          direction="row"
+          padding={2}
+          sx={{
+            position: "absolute",
+            bottom: 0,
+            backgroundColor: "primary.main",
+            color: "#FFFFFF"
+          }}
+        >
+          <Typography textAlign="center" fontWeight="bolder">
+            {title}
+          </Typography>
+        </Box>
       </Stack>
-      <Box
-        border={1}
-        height={40}
-        width="100%"
-        textAlign="center"
-        component={Stack}
-        alignItems="center"
-        justifyContent="center"
-        direction="row"
-        sx={{
-          position: "absolute",
-          bottom: 0,
-          backgroundColor: "primary.main",
-          color: "#FFFFFF"
-        }}
-      >
-        <Typography textAlign="center" fontWeight="bolder">
-          {title}
-        </Typography>
-      </Box>
-    </Stack>
+    </Card>
   );
 };
 

d9c3486579c17e8cf75d30dcbf2d313881eab26b
Kamzen <kamzen1994@gmail.com>
2023-06-30 11:54:10 +0200

    Create ProfessionalBodies.jsx

---
+++ src/pages/ProfessionalBodies/ProfessionalBodies.jsx
@@ -0,0 +1,107 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import AdjustIcon from "@mui/icons-material/Adjust";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import SkillsProgramme from "../../components/Employers/SkillsProgramme";
+import WorkbasedProgramme from "../../components/Employers/WorkbasedProgramme";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+      style={{ width: "80%" }}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`
+  };
+}
+
+const ProfessionalBodies = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        bgcolor: "background.paper",
+        display: "flex",
+        height: 450
+      }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        textColor="secondary"
+        indicatorColor="secondary"
+        sx={{
+          borderRight: 1,
+          borderColor: "lightgray",
+          borderBottomRightRadius: 10,
+          backgroundColor: "primary.main"
+        }}
+        TabIndicatorProps={{
+          sx: {
+            border: 3,
+            borderColor: "secondary.main"
+          }
+        }}
+      >
+        <Tab
+          label="About FASSET Learnerships"
+          icon={<AdjustIcon />}
+          iconPosition="start"
+          {...a11yProps(0)}
+          sx={{ borderBottom: 1, borderColor: "lightgray", color: "#FFFFFF", px: 4 }}
+        />
+        <Tab
+          label="Lifelong Learning Events"
+          icon={<EditNoteIcon />}
+          iconPosition="start"
+          {...a11yProps(1)}
+          sx={{ borderBottom: 1, borderColor: "lightgray", color: "#FFFFFF" }}
+        />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <SkillsProgramme />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <WorkbasedProgramme />
+      </TabPanel>
+    </Box>
+  );
+};
+
+export default ProfessionalBodies;

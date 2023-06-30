bfc0800520ad302c6f20a713e442249c05a8b3bd
Kamzen <kamzen1994@gmail.com>
2023-06-30 11:52:18 +0200

    Create CancelledTenders.jsx

---
+++ src/components/SupplyChain/CancelledTenders.jsx
@@ -0,0 +1,168 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import {
+  Chip,
+  IconButton,
+  Stack,
+  TableFooter,
+  TablePagination,
+  Tooltip
+} from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import DownloadIcon from "@mui/icons-material/Download";
+import TablePaginationActions from "@mui/material/TablePagination/TablePaginationActions";
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9)
+];
+
+const CancelledTenders = () => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <Stack paddingX={10}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead sx={{ backgroundColor: "primary.main" }}>
+            <TableRow>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                Tender Name
+              </TableCell>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                Tender Code
+              </TableCell>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                Date Posted
+              </TableCell>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                Closing Date
+              </TableCell>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                Status
+              </TableCell>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                Actions
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : rows
+            ).map((row, i) => (
+              <TableRow
+                key={row.name}
+                sx={{ backgroundColor: i % 2 === 0 ? "action.hover" : "" }}
+              >
+                <TableCell align="center" component="th" scope="row">
+                  NETWORK UPGRADE
+                </TableCell>
+                <TableCell align="center" component="th" scope="row">
+                  FAS/TM/ICT/NET-INFRA-UPGRADE/CON3254/23
+                </TableCell>
+                <TableCell align="center" component="th" scope="row">
+                  29 June 2023
+                </TableCell>
+                <TableCell align="center" scope="row">
+                  30 July 2022
+                </TableCell>
+                <TableCell align="center" scope="row">
+                  <Chip label="Cancelled" color="error" />
+                </TableCell>
+
+                <TableCell align="center" scope="row">
+                  <Stack direction="row">
+                    <Tooltip title="View">
+                      <IconButton color="secondary">
+                        <VisibilityIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Download">
+                      <IconButton color="secondary">
+                        <DownloadIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </Stack>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                // colSpan={3}
+                count={rows?.length || 0}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    "aria-label": "rows per page"
+                  },
+                  native: true
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </Stack>
+  );
+};
+
+export default CancelledTenders;

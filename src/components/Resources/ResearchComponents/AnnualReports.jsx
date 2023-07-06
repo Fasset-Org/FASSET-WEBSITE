7b5e8bb16395911eef59469a5bb8be65aa00d365
Kamzen <kamzen1994@gmail.com>
2023-07-06 16:42:20 +0200

    Create AnnualReports.jsx

---
+++ src/components/Resources/ResearchComponents/AnnualReports.jsx
@@ -0,0 +1,20 @@
+import { Stack, Typography } from "@mui/material";
+import React from "react";
+
+const AnnualReports = () => {
+  return (
+    <Stack>
+      <Typography
+        textAlign="center"
+        fontFamily="Helvetica Neue"
+        fontWeight="bolder"
+        fontSize={20}
+        sx={{ color: "primary.main" }}
+      >
+        Annual Reports
+      </Typography>
+    </Stack>
+  );
+};
+
+export default AnnualReports;

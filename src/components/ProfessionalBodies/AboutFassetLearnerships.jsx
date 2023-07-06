0f2bf7e27c50efb1faefc493c825820407af382d
Kamzen <kamzen1994@gmail.com>
2023-07-06 16:42:31 +0200

    Update AboutFassetLearnerships.jsx

--- src/components/ProfessionalBodies/AboutFassetLearnerships.jsx
+++ src/components/ProfessionalBodies/AboutFassetLearnerships.jsx
@@ -1,9 +1,151 @@
-import React from "react";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Stack, TableFooter, TablePagination, Typography } from "@mui/material";
+import TablePaginationActions from "@mui/material/TablePagination/TablePaginationActions";
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9)
+];
 
 const AboutFassetLearnerships = () => {
-  return (<div>
-    AboutFassetLearnerships
-    </div>);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <Stack spacing={2} justifyContent="center">
+      <Typography
+        textAlign="center"
+        fontFamily="Helvetica Neue"
+        fontWeight="bolder"
+        fontSize={20}
+        sx={{ color: "primary.main" }}
+      >
+        About Fasset Learnerships
+      </Typography>
+      <Typography fontWeight="bolder">
+        Association of Accounting Technicians (AAT(SA)) 011 621 6888 –
+        info@aatsa.org.za
+      </Typography>
+      <TableContainer component={Paper} sx={{ borderRadius: 0 }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead sx={{ backgroundColor: "primary.main" }}>
+            <TableRow>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                No#
+              </TableCell>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                Title
+              </TableCell>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                NQF Level
+              </TableCell>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                Learnership Code
+              </TableCell>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                SAQA ID No
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : rows
+            ).map((row, i) => (
+              <TableRow
+                key={row.name}
+                sx={{ backgroundColor: i % 2 === 0 ? "action.hover" : "" }}
+              >
+                <TableCell align="center" component="th" scope="row">
+                  {i + 1}
+                </TableCell>
+                <TableCell align="center" component="th" scope="row">
+                  Certificate: Local Government Accounting
+                </TableCell>
+                <TableCell align="center" component="th" scope="row">
+                  3
+                </TableCell>
+                <TableCell align="center" scope="row">
+                  01/Q010026/00/120/3
+                </TableCell>
+                <TableCell align="center" scope="row">
+                  59751
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                // colSpan={3}
+                count={rows?.length || 0}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    "aria-label": "rows per page"
+                  },
+                  native: true
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </Stack>
+  );
 };
 
 export default AboutFassetLearnerships;

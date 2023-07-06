1fda9e2309f5e7bb3430cd74fd0ab8db59f4e706
Kamzen <kamzen1994@gmail.com>
2023-07-06 16:43:15 +0200

    Create SectorTrends.jsx

---
+++ src/components/Resources/ResearchComponents/SectorTrends.jsx
@@ -0,0 +1,205 @@
+import {
+  IconButton,
+  Menu,
+  MenuItem,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography
+} from "@mui/material";
+import React from "react";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import TablePaginationActions from "@mui/material/TablePagination/TablePaginationActions";
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9)
+];
+
+const SectorTrends = () => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Stack spacing={1}>
+      <Typography
+        textAlign="center"
+        fontFamily="Helvetica Neue"
+        fontWeight="bolder"
+        fontSize={20}
+        sx={{ color: "primary.main" }}
+      >
+        Sector Trends
+      </Typography>
+      <Typography>
+        The Finance and Accounting Services (FAS) Sector Trend report is an
+        annual analysis that identifies trends in the training and employment
+        FAS sector to provide context to skills development needs. This
+        publication provides quantitative/statistical as well as qualitative
+        analyses covering demographic profile, employment trends, training etc.,
+        in the FAS sector. The FAS Sector Trends study’s purpose is to collect
+        labour market information to inform skills planning and funding
+        decisions of the Financial and Accounting Services Sector Education and
+        Training Authority (FASSET) as well as to monitor transformation in the
+        sector.
+      </Typography>
+
+      <TableContainer component={Paper} sx={{ borderRadius: 0 }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead sx={{ backgroundColor: "primary.main" }}>
+            <TableRow>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                No#
+              </TableCell>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                Year
+              </TableCell>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                Document Title
+              </TableCell>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                Document Description
+              </TableCell>
+              <TableCell
+                align="center"
+                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
+              >
+                Action
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : rows
+            ).map((row, i) => (
+              <TableRow
+                key={row.name}
+                sx={{ backgroundColor: i % 2 === 0 ? "action.hover" : "" }}
+              >
+                <TableCell align="center" component="th" scope="row">
+                  {i + 1}
+                </TableCell>
+                <TableCell align="center" component="th" scope="row">
+                  2021
+                </TableCell>
+                <TableCell align="center" component="th" scope="row">
+                  Sector Trends Study
+                </TableCell>
+                <TableCell align="center" component="th" scope="row">
+                  Trends in Employment and Training in the FASSET Sector
+                </TableCell>
+
+                <TableCell align="center" scope="row">
+                  <IconButton
+                    id="demo-positioned-button"
+                    aria-controls={open ? "demo-positioned-menu" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? "true" : undefined}
+                    onClick={handleClick}
+                  >
+                    <MoreVertIcon />
+                  </IconButton>
+                  <Menu
+                    id="demo-positioned-menu"
+                    aria-labelledby="demo-positioned-button"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "left"
+                    }}
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "left"
+                    }}
+                  >
+                    <MenuItem onClick={handleClose}>View</MenuItem>
+                    <MenuItem onClick={handleClose}>Download</MenuItem>
+                  </Menu>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                // colSpan={3}
+                count={rows?.length || 0}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    "aria-label": "rows per page"
+                  },
+                  native: true
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </Stack>
+  );
+};
+
+export default SectorTrends;

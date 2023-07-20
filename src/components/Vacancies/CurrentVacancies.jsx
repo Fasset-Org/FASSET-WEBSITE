1b6603db5d75047a1e68d806e85b6bf4356c25e8
Kamzen <kamzen1994@gmail.com>
2023-07-20 06:37:40 +0200

    Create CurrentVacancies.jsx

---
+++ src/components/Vacancies/CurrentVacancies.jsx
@@ -0,0 +1,58 @@
+
+import { Grid } from "@mui/material";
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import VacancyCard from "../../components/VacancyCard";
+
+const CurrentVacancies = () => {
+  const vacancies = [
+    {
+      id: uuidv4(),
+      jobTitle: "Frontend Developer C1",
+      jobDescription:
+        "An opportunity to work abroad has been made available with one of the leading Travel Business that provide strategic solutions to clients with high quality",
+      jobRem: "650K"
+    },
+    {
+      id: uuidv4(),
+      jobTitle: "Frontend Developer C1",
+      jobDescription:
+        "An opportunity to work abroad has been made available with one of the leading Travel Business that provide strategic solutions to clients with high quality",
+      jobRem: "650K"
+    },
+    {
+      id: uuidv4(),
+      jobTitle: "Frontend Developer C1",
+      jobDescription:
+        "An opportunity to work abroad has been made available with one of the leading Travel Business that provide strategic solutions to clients with high quality",
+      jobRem: "650K"
+    },
+    {
+      id: uuidv4(),
+      jobTitle: "Frontend Developer C1",
+      jobDescription:
+        "An opportunity to work abroad has been made available with one of the leading Travel Business that provide strategic solutions to clients with high quality",
+      jobRem: "650K"
+    },
+    {
+      id: uuidv4(),
+      jobTitle: "Frontend Developer C1",
+      jobDescription:
+        "An opportunity to work abroad has been made available with one of the leading Travel Business that provide strategic solutions to clients with high quality",
+      jobRem: "650K"
+    }
+  ];
+
+  return (
+    <Grid
+      container
+      spacing={2}
+    >
+      {vacancies.map((vacancy, i) => {
+        return <VacancyCard vacancy={vacancy} />;
+      })}
+    </Grid>
+  );
+};
+
+export default CurrentVacancies;

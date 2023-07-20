8feedc0450f48780d0fecef437d5a896abe211ab
Kamzen <kamzen1994@gmail.com>
2023-07-20 06:37:32 +0200

    Update Vacancies.jsx

--- src/pages/Vacancies/Vacancies.jsx
+++ src/pages/Vacancies/Vacancies.jsx
@@ -1,58 +1,116 @@
-import { Grid } from "@mui/material";
-import React from "react";
-import { v4 as uuidv4 } from "uuid";
-import VacancyCard from "../../components/VacancyCard";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
+import RoomPreferencesIcon from "@mui/icons-material/RoomPreferences";
+import OurMandate from "../../components/AboutUs/OurMandate";
+import CurrentVacancies from "../../components/Vacancies/CurrentVacancies";
 
-const Vacancies = () => {
-  const vacancies = [
-    {
-      id: uuidv4(),
-      jobTitle: "Frontend Developer C1",
-      jobDescription:
-        "An opportunity to work abroad has been made available with one of the leading Travel Business that provide strategic solutions to clients with high quality",
-      jobRem: "650K"
-    },
-    {
-      id: uuidv4(),
-      jobTitle: "Frontend Developer C1",
-      jobDescription:
-        "An opportunity to work abroad has been made available with one of the leading Travel Business that provide strategic solutions to clients with high quality",
-      jobRem: "650K"
-    },
-    {
-      id: uuidv4(),
-      jobTitle: "Frontend Developer C1",
-      jobDescription:
-        "An opportunity to work abroad has been made available with one of the leading Travel Business that provide strategic solutions to clients with high quality",
-      jobRem: "650K"
-    },
-    {
-      id: uuidv4(),
-      jobTitle: "Frontend Developer C1",
-      jobDescription:
-        "An opportunity to work abroad has been made available with one of the leading Travel Business that provide strategic solutions to clients with high quality",
-      jobRem: "650K"
-    },
-    {
-      id: uuidv4(),
-      jobTitle: "Frontend Developer C1",
-      jobDescription:
-        "An opportunity to work abroad has been made available with one of the leading Travel Business that provide strategic solutions to clients with high quality",
-      jobRem: "650K"
-    }
-  ];
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
   return (
-    <Grid
-      container
-      spacing={10}
-      sx={{ px: { xs: 2, sm: 2, md: 10, lg: 10, xl: 10 } }}
-      paddingY={4}
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+      style={{ width: "80%" }}
     >
-      {vacancies.map((vacancy, i) => {
-        return <VacancyCard vacancy={vacancy} />;
-      })}
-    </Grid>
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`
+  };
+}
+
+const Vacancies = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        bgcolor: "background.paper",
+        display: "flex",
+      }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        textColor="secondary"
+        sx={{
+          borderRight: 1,
+          borderColor: "lightgray",
+          borderRadius: 2,
+          backgroundColor: "primary.main",
+          height: 450,
+          mt: 3,
+          ml:3
+        }}
+        TabIndicatorProps={{
+          sx: {
+            borderBottom: 1,
+            borderColor: "secondary.main"
+          }
+        }}
+      >
+        <Tab
+          label="Available Vacancies"
+          icon={<AssignmentIndIcon />}
+          iconPosition="start"
+          {...a11yProps(0)}
+          sx={{
+            borderBottom: 1,
+            borderColor: "lightgray",
+            color: "#FFFFFF"
+          }}
+        />
+        <Tab
+          label="Previous Vacancies"
+          icon={<RoomPreferencesIcon />}
+          iconPosition="start"
+          {...a11yProps(1)}
+          sx={{
+            borderBottom: 1,
+            borderColor: "lightgray",
+            color: "#FFFFFF",
+            textAlign: "start"
+          }}
+        />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <CurrentVacancies />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <OurMandate />
+      </TabPanel>
+    </Box>
   );
 };
 

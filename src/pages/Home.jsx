fadf85c6e0e924cb8d9f05c8b48686625d1770b6
Kamzen <kamzen1994@gmail.com>
2023-07-27 15:35:24 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Stack, Typography } from "@mui/material";
+import { Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
 import LatestNews from "../components/Latest News/LatestNews";
 import banner1 from "../images/bgImage1.avif";
 import banner2 from "../images/Website-Deadline-Extension.png";
@@ -13,58 +13,73 @@ import NoticeBoardModal from "../components/NoticeBoardModal";
 const Home = ({ id }) => {
   const images2 = [banner1, banner2, banner3, banner4];
 
+  const theme = useTheme();
+  const xs = useMediaQuery(theme.breakpoints.down("xs"));
+  const sm = useMediaQuery(theme.breakpoints.down("sm"));
+  const md = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
     <Stack spacing={2} width="100%">
       {/* <DesktopBanner /> */}
-      <Slideshow images={images2} height="500px" />
 
-      <NoticeBoardModal />
+      {xs ||
+        sm ||
+        (md ? (
+          <>
+            <Stack height={500} sx={{bgcolor: 'red'}}>Hi</Stack>
+          </>
+        ) : (
+          <>
+            <Slideshow images={images2} height="400px" />
 
-      <Stack>
-        <FassetAssistant />
-      </Stack>
+            <NoticeBoardModal />
 
-      <Typography
-        fontSize={30}
-        fontWeight="bolder"
-        textTransform="uppercase"
-        fontFamily="Helvetica Neue"
-        textAlign="center"
-        sx={{ color: "primary.main" }}
-      >
-        Values
-      </Typography>
+            <Stack>
+              <FassetAssistant />
+            </Stack>
 
-      <Stack direction="row" spacing={2} padding={2}>
-        <ValueCard
-          title="Professionalism And Accountability"
-          definition={
-            "We are a high-performance, professional team; we strive for excellence through hard work and by taking responsibility and being accountable within an accepted and agreed code of conduct."
-          }
-        />
-        <ValueCard
-          title="Ethics"
-          definition="We work with integrity and honesty and respect internal and external stakeholders at all times."
-        />
-        <ValueCard
-          title="Making A Difference"
-          definition={
-            "We make a difference to the lives of all our stakeholders by identifying their needs and exceeding expectations."
-          }
-        />
-        <ValueCard
-          title="Valuing People"
-          definition={
-            "We strive to create a motivating and supportive culture by understanding, respecting, developing and valuing each other."
-          }
-        />
-        <ValueCard
-          title="Innovation"
-          definition="We Continuously innovate and improve to add value to all our stakeholders. The Work of Setas"
-        />
-        <LatestNews />
-      </Stack>
-      
+            <Typography
+              fontSize={30}
+              fontWeight="bolder"
+              textTransform="uppercase"
+              fontFamily="Helvetica Neue"
+              textAlign="center"
+              sx={{ color: "primary.main" }}
+            >
+              Values
+            </Typography>
+
+            <Stack direction="row" spacing={2} padding={2}>
+              <ValueCard
+                title="Professionalism And Accountability"
+                definition={
+                  "We are a high-performance, professional team; we strive for excellence through hard work and by taking responsibility and being accountable within an accepted and agreed code of conduct."
+                }
+              />
+              <ValueCard
+                title="Ethics"
+                definition="We work with integrity and honesty and respect internal and external stakeholders at all times."
+              />
+              <ValueCard
+                title="Making A Difference"
+                definition={
+                  "We make a difference to the lives of all our stakeholders by identifying their needs and exceeding expectations."
+                }
+              />
+              <ValueCard
+                title="Valuing People"
+                definition={
+                  "We strive to create a motivating and supportive culture by understanding, respecting, developing and valuing each other."
+                }
+              />
+              <ValueCard
+                title="Innovation"
+                definition="We Continuously innovate and improve to add value to all our stakeholders. The Work of Setas"
+              />
+              <LatestNews />
+            </Stack>
+          </>
+        ))}
     </Stack>
   );
 };

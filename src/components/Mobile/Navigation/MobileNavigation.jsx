7cbc29678224e2bfe212f1fc41859262e92e845a
Kamzen <kamzen1994@gmail.com>
2023-07-31 13:18:27 +0200

    Update MobileNavigation.jsx

--- src/components/Mobile/Navigation/MobileNavigation.jsx
+++ src/components/Mobile/Navigation/MobileNavigation.jsx
@@ -63,6 +63,7 @@ import SummarizeIcon from "@mui/icons-material/Summarize";
 import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
 import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
 import { Outlet } from "react-router-dom";
+import LinksFooter from "../../LinksFooter";
 // import TopContactNavigation from "../../Navigation/TopContactNavigation";
 
 const drawerWidth = 280;
@@ -277,7 +278,7 @@ export default function MobileNavigation() {
         <DrawerHeader />
 
         <Outlet />
-        {/* <LinksFooter /> */}
+        <LinksFooter />
       </Box>
     </Stack>
   );

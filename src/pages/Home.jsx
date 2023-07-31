dd92c3f7812ed7bce60653c2d22174c76056ba36
Kamzen <kamzen1994@gmail.com>
2023-07-31 13:18:18 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,6 +25,52 @@ const Home = ({ id }) => {
       {xs || sm || md ? (
         <>
           <Slideshow images={images2} height="300px" />
+          <FassetAssistant />
+
+          <Typography
+            fontSize={30}
+            fontWeight="bolder"
+            textTransform="uppercase"
+            fontFamily="Helvetica Neue"
+            textAlign="center"
+            sx={{ color: "primary.main" }}
+          >
+            Values
+          </Typography>
+
+          <Stack
+            direction='column'
+            spacing={2}
+            padding={2}
+          >
+            <ValueCard
+              title="Professionalism And Accountability"
+              definition={
+                "We are a high-performance, professional team; we strive for excellence through hard work and by taking responsibility and being accountable within an accepted and agreed code of conduct."
+              }
+            />
+            <ValueCard
+              title="Ethics"
+              definition="We work with integrity and honesty and respect internal and external stakeholders at all times."
+            />
+            <ValueCard
+              title="Making A Difference"
+              definition={
+                "We make a difference to the lives of all our stakeholders by identifying their needs and exceeding expectations."
+              }
+            />
+            <ValueCard
+              title="Valuing People"
+              definition={
+                "We strive to create a motivating and supportive culture by understanding, respecting, developing and valuing each other."
+              }
+            />
+            <ValueCard
+              title="Innovation"
+              definition="We Continuously innovate and improve to add value to all our stakeholders. The Work of Setas"
+            />
+            <LatestNews />
+          </Stack>
         </>
       ) : (
         <>
@@ -47,7 +93,11 @@ const Home = ({ id }) => {
             Values
           </Typography>
 
-          <Stack direction="row" spacing={2} padding={2}>
+          <Stack
+            direction='row'
+            spacing={2}
+            padding={2}
+          >
             <ValueCard
               title="Professionalism And Accountability"
               definition={

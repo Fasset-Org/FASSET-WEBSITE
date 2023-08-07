35deb32e7554a1aa81d953902e14479db0a4b0b2
Kamzen <kamzen1994@gmail.com>
2023-08-07 08:41:25 +0200

    Update Committees.jsx

--- src/components/AboutUs/Committees.jsx
+++ src/components/AboutUs/Committees.jsx
@@ -41,7 +41,7 @@ const Committees = () => {
         fontFamily="Helvetica Neue"
         fontWeight="bolder"
         fontSize={20}
-        sx={{ color: "primary.main" }}
+        sx={{ color: "primary.main", textTransform: 'uppercase' }}
       >
         Executive Members
       </Typography>

9484135f6801820f6866548a1040762f18640bde
Kamzen <kamzen1994@gmail.com>
2023-08-07 17:09:32 +0200

    Create WhoWeAreMobile.jsx

---
+++ src/pages/Mobile/AboutUs/WhoWeAreMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import WhoWeAre from '../../../components/AboutUs/WhoWeAre'
+
+const WhoWeAreMobile = () => {
+  return (
+   <Stack padding={2}>
+    <WhoWeAre />
+   </Stack>
+  )
+}
+
+export default WhoWeAreMobile

3b731c6d043a923c29cd1bebacb66e977b9ddfc3
Kamzen <kamzen1994@gmail.com>
2023-08-07 17:09:15 +0200

    Create BoardMobile.jsx

---
+++ src/pages/Mobile/AboutUs/BoardMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import BoardMembers from '../../../components/AboutUs/BoardMembers'
+
+const BoardMobile = () => {
+  return (
+    <Stack padding={2}>
+      <BoardMembers />
+    </Stack>
+  )
+}
+
+export default BoardMobile

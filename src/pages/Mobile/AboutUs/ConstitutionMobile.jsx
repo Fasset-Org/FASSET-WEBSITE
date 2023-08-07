c8b64342ed49f8088389f929740fd026861b4d2b
Kamzen <kamzen1994@gmail.com>
2023-08-07 17:09:21 +0200

    Create ConstitutionMobile.jsx

---
+++ src/pages/Mobile/AboutUs/ConstitutionMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import Constitution from '../../../components/AboutUs/Constitution'
+
+const ConstitutionMobile = () => {
+  return (
+    <Stack padding={2}>
+      <Constitution />
+    </Stack>
+  )
+}
+
+export default ConstitutionMobile

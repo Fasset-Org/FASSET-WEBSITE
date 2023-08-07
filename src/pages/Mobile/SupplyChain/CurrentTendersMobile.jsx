6dc207e22b922ac4d2c8333b07247542d1e70391
Kamzen <kamzen1994@gmail.com>
2023-08-07 17:10:17 +0200

    Create CurrentTendersMobile.jsx

---
+++ src/pages/Mobile/SupplyChain/CurrentTendersMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import ActiveTenders from '../../../components/SupplyChain/ActiveTenders'
+
+const CurrentTendersMobile = () => {
+  return (
+    <Stack padding={2}>
+      <ActiveTenders />
+    </Stack>
+  )
+}
+
+export default CurrentTendersMobile

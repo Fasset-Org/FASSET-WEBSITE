704209bb26441d82c28cb9a3b9e376be7bab2efe
Kamzen <kamzen1994@gmail.com>
2023-08-07 07:56:57 +0200

    Update ValueCard.jsx

--- src/components/ValueCard.jsx
+++ src/components/ValueCard.jsx
@@ -5,23 +5,23 @@ const ValueCard = ({ title, definition }) => {
   return (
     <Card
       className="v-card"
-      sx={{ minHeight: 260, width: { xs: "100%", md: "20%" }, borderRadius: 0, p: 0 }}
+      sx={{ minHeight: 190, width: { xs: "100%", md: "20%" }, borderRadius: 0, p: 0 }}
     >
       <Stack height="100%" sx={{ position: "relative" }}>
         <Stack
-          height={210}
+          height={190}
           alignItems="center"
           justifyContent="center"
           direction="row"
           position="absolute"
           top={0}
           padding={2}
-          border={1}
+          // border={1}
         >
           <Typography textAlign="center">{definition}</Typography>
         </Stack>
         <Box
-          border={1}
+          // border={1}
           height={40}
           width="100%"
           textAlign="center"

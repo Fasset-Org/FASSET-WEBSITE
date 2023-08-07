81e481eceb2b6b4802ab2acdd12888a5c8e437ff
Kamzen <kamzen1994@gmail.com>
2023-08-07 17:09:51 +0200

    Create BursaryMobile.jsx

---
+++ src/pages/Mobile/Learners/BursaryMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from "@mui/material";
+import React from "react";
+
+const BursaryMobile = () => {
+  return (
+    <Stack padding={2}>
+      About Fasset Bursary will be defined here and provide a link to the
+      bursary system
+    </Stack>
+  );
+};
+
+export default BursaryMobile;

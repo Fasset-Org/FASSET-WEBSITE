6c6d1c9f04042efa63e717297a4fbf2102e72240
Kamzen <kamzen1994@gmail.com>
2023-08-07 07:57:24 +0200

    Update Constitution.jsx

--- src/components/AboutUs/Constitution.jsx
+++ src/components/AboutUs/Constitution.jsx
@@ -1,7 +1,7 @@
-import { Button, Paper, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import React from "react";
-import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
-import { Download } from "@mui/icons-material";
+import DownloadCard from "../DownloadCard";
+
 
 const Constitution = () => {
   return (
@@ -12,19 +12,9 @@ const Constitution = () => {
         fontSize={20}
         sx={{ color: "primary.main" }}
       >
-        Fasset Constitution
+         Constitution
       </Typography>
-      <Stack sx={{ width: 200 }} component={Paper} padding={2}>
-        <Stack spacing={2} alignItems="center" m="auto">
-          <PictureAsPdfIcon fontSize="large" sx={{ color: "primary.main" }} />
-          <Typography fontWeight="bolder" fontSize={15}>
-            Fasset's Constitution
-          </Typography>
-          <Button variant="outlined" startIcon={<Download />}>
-            Download
-          </Button>
-        </Stack>
-      </Stack>
+      <DownloadCard fileName={"Fasset's Constitution"} />
     </Stack>
   );
 };

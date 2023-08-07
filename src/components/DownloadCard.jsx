b238019cd93bad3204a0a0e37c7125549d16e856
Kamzen <kamzen1994@gmail.com>
2023-08-07 07:57:19 +0200

    Create DownloadCard.jsx

---
+++ src/components/DownloadCard.jsx
@@ -0,0 +1,22 @@
+import { Button, Paper, Stack, Typography } from "@mui/material";
+import React from "react";
+import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
+import { Download } from "@mui/icons-material";
+
+const DownloadCard = ({fileName}) => {
+  return (
+    <Stack sx={{ width: 200 }} component={Paper} padding={2}>
+        <Stack spacing={2} alignItems="center" m="auto">
+          <PictureAsPdfIcon fontSize="large" sx={{ color: "primary.main" }} />
+          <Typography fontWeight="bolder" fontSize={15}>
+            {fileName}
+          </Typography>
+          <Button variant="outlined" startIcon={<Download />}>
+            Download
+          </Button>
+        </Stack>
+      </Stack>
+  )
+}
+
+export default DownloadCard

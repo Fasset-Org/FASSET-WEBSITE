ce1bf0b9881eac820a44f904d0470da05577bd78
Kamzen <kamzen1994@gmail.com>
2023-08-07 17:09:36 +0200

    Create DiscretionaryGrantsMobile.jsx

---
+++ src/pages/Mobile/Employers/DiscretionaryGrantsMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import DiscretionaryGrants from '../../../components/Employers/DiscretionaryGrants'
+
+const DiscretionaryGrantsMobile = () => {
+  return (
+    <Stack padding={2}>
+      <DiscretionaryGrants />
+    </Stack>
+  )
+}
+
+export default DiscretionaryGrantsMobile

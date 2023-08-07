f95abe3e1cfbe2b4de501ad79534c901eb039fd7
Kamzen <kamzen1994@gmail.com>
2023-08-07 17:09:39 +0200

    Create MandatoryGrantsMobile.jsx

---
+++ src/pages/Mobile/Employers/MandatoryGrantsMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import MandatoryGrants from '../../../components/Employers/MandatoryGrants'
+
+const MandatoryGrantsMobile = () => {
+  return (
+    <Stack padding={2}>
+      <MandatoryGrants />
+    </Stack>
+  )
+}
+
+export default MandatoryGrantsMobile

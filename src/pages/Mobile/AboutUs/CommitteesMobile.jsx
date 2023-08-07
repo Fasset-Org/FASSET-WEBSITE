93f7094765fe67e90b4c66aa4f77ded40d4b936c
Kamzen <kamzen1994@gmail.com>
2023-08-07 17:09:18 +0200

    Create CommitteesMobile.jsx

---
+++ src/pages/Mobile/AboutUs/CommitteesMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import Committees from '../../../components/AboutUs/Committees'
+
+const CommitteesMobile = () => {
+  return (
+    <Stack padding={2}>
+      <Committees />
+    </Stack>
+  )
+}
+
+export default CommitteesMobile

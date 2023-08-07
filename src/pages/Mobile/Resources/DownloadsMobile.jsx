0c52cf907ded1a66cba8aeb5f344105cb3382e45
Kamzen <kamzen1994@gmail.com>
2023-08-07 17:10:05 +0200

    Create DownloadsMobile.jsx

---
+++ src/pages/Mobile/Resources/DownloadsMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import Downloads from '../../../components/Resources/Downloads'
+
+const DownloadsMobile = () => {
+  return (
+    <Stack padding={2}>
+      <Downloads />
+    </Stack>
+  )
+}
+
+export default DownloadsMobile

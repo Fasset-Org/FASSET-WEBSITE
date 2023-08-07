ae7c84cdda75bd56b11229af5a754fcc52fca5ca
Kamzen <kamzen1994@gmail.com>
2023-08-07 17:09:45 +0200

    Create TrainingProvidersMobile.jsx

---
+++ src/pages/Mobile/Employers/TrainingProvidersMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import TrainingProviders from '../../../components/Employers/TrainingProviders'
+
+const TrainingProvidersMobile = () => {
+  return (
+    <Stack padding={2}>
+      <TrainingProviders />
+    </Stack>
+  )
+}
+
+export default TrainingProvidersMobile

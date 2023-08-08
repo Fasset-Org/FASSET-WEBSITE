7f4be9fa5df1358c10a8315c2b36c06f14701ba8
Kamzen <kamzen1994@gmail.com>
2023-08-08 11:41:33 +0200

    Update ActiveTenders.jsx

--- src/components/SupplyChain/ActiveTenders.jsx
+++ src/components/SupplyChain/ActiveTenders.jsx
@@ -1,219 +1,27 @@
 import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import {
-  Chip,
-  IconButton,
-  Stack,
-  TableFooter,
-  TablePagination,
-  Tooltip,
-  Typography
-} from "@mui/material";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import DownloadIcon from "@mui/icons-material/Download";
-import TablePaginationActions from "@mui/material/TablePagination/TablePaginationActions";
-import { useNavigate } from "react-router-dom";
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9)
-];
+import { Grid, Stack, Typography } from "@mui/material";
+import TenderCard from "./TenderCard";
 
 const ActiveTenders = () => {
-  const navigate = useNavigate();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
+  
   return (
-    <Stack spacing={2}>
+    <Stack>
       <Typography
-        fontFamily="Helvetica Neue"
         fontWeight="bolder"
         fontSize={20}
-        sx={{ color: "primary.main", textTransform: "uppercase" }}
+        sx={{ color: "primary.main", textTransform: "uppercase", mb: 2 }}
       >
         Current Tenders
       </Typography>
-      {/* <Grid container spacing={2}>
-        <Grid item xs={12} md={4}>
-          <Stack
-            component={Paper}
-            height={300}
-            width="100%"
-            spacing={1}
-          >
-            <Stack
-              height={40}
-              width="100%"
-              sx={{ backgroundColor: "primary.main", color: "#FFFFFF" }}
-              justifyContent="center"
-              alignItems="center"
-            >
-              NETWORK UPGRADE
-            </Stack>
-
-            <Stack justifyContent="center" alignItems="center">
-              <Typography fontSize={12} fontWeight="bolder">
-                FAS/TM/ICT/NET-INFRA-UPGRADE/CON3254/23
-              </Typography>
-            </Stack>
-          </Stack>
-        </Grid>
-
-        <Grid item xs={12} md={4}>
-          <Stack component={Paper} height={300} width="100%"></Stack>
-        </Grid>
-
-        <Grid item xs={12} md={4}>
-          <Stack component={Paper} height={300} width="100%"></Stack>
-        </Grid>
-
-        <Grid item xs={12} md={4}>
-          <Stack component={Paper} height={300} width="100%"></Stack>
-        </Grid>
-      </Grid> */}
-
-      <TableContainer component={Paper} sx={{ borderRadius: 0 }}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead sx={{ backgroundColor: "primary.main" }}>
-            <TableRow>
-              <TableCell
-                align="center"
-                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
-              >
-                Tender Name
-              </TableCell>
-              <TableCell
-                align="center"
-                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
-              >
-                Tender Reference
-              </TableCell>
-              <TableCell
-                align="center"
-                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
-              >
-                Date Posted
-              </TableCell>
-              <TableCell
-                align="center"
-                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
-              >
-                Closing Date
-              </TableCell>
-              <TableCell
-                align="center"
-                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
-              >
-                Status
-              </TableCell>
-              <TableCell
-                align="center"
-                sx={{ fontWeight: "bolder", color: "#FFFFFF" }}
-              >
-                Actions
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {(rowsPerPage > 0
-              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              : rows
-            ).map((row, i) => (
-              <TableRow
-                key={row.name}
-                sx={{ backgroundColor: i % 2 === 0 ? "action.hover" : "" }}
-              >
-                <TableCell align="center" component="th" scope="row">
-                  NETWORK UPGRADE
-                </TableCell>
-                <TableCell align="center" component="th" scope="row">
-                  FAS/TM/ICT/NET-INFRA-UPGRADE/CON3254/23
-                </TableCell>
-                <TableCell align="center" component="th" scope="row">
-                  29 June 2023
-                </TableCell>
-                <TableCell align="center" scope="row">
-                  30 July 2022
-                </TableCell>
-                <TableCell align="center" scope="row">
-                  <Chip label="Active" color="success" />
-                </TableCell>
-
-                <TableCell align="center" scope="row">
-                  <Stack direction="row">
-                    <Tooltip title="View">
-                      <IconButton
-                        color="secondary"
-                        onClick={() => navigate(`/tenders/${1}`)}
-                      >
-                        <VisibilityIcon />
-                      </IconButton>
-                    </Tooltip>
-                    <Tooltip title="Download">
-                      <IconButton color="secondary">
-                        <DownloadIcon />
-                      </IconButton>
-                    </Tooltip>
-                  </Stack>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
-                // colSpan={3}
-                count={rows?.length || 0}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                SelectProps={{
-                  inputProps: {
-                    "aria-label": "rows per page"
-                  },
-                  native: true
-                }}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationActions}
-              />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </TableContainer>
+      <Grid container spacing={2}>
+        {[...Array(5)].map((option, i) => {
+          return (
+            <Grid key={i} xs={12} md={6} item >
+              <TenderCard />
+            </Grid>
+          );
+        })}
+      </Grid>
     </Stack>
   );
 };

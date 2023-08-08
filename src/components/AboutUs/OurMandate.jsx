3ae94ea61fbb17d4907b655d60955525ec40551b
Kamzen <kamzen1994@gmail.com>
2023-08-08 11:42:16 +0200

    Update OurMandate.jsx

--- src/components/AboutUs/OurMandate.jsx
+++ src/components/AboutUs/OurMandate.jsx
@@ -195,7 +195,6 @@ const OurMandate = () => {
   return (
     <Stack spacing={1}>
       <Typography
-        fontFamily="Helvetica Neue"
         fontWeight="bolder"
         fontSize={20}
         sx={{ color: "primary.main", textTransform: 'uppercase' }}

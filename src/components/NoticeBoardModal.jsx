0dc12fa5ed8c60095b92182570cf5593431977bc
Kamzen <kamzen1994@gmail.com>
2023-08-09 04:22:03 +0200

    Update NoticeBoardModal.jsx

--- src/components/NoticeBoardModal.jsx
+++ src/components/NoticeBoardModal.jsx
@@ -1,25 +1,15 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import Button from "@mui/material/Button";
-import { styled, useTheme } from "@mui/material/styles";
+import { useTheme } from "@mui/material/styles";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
-import Typography from "@mui/material/Typography";
 import { Slide, useMediaQuery } from "@mui/material";
-import { motion } from "framer-motion";
-
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root": {
-    padding: theme.spacing(2)
-  },
-  "& .MuiDialogActions-root": {
-    padding: theme.spacing(1)
-  }
-}));
+import WindowCountDown from "./WindowCountDown";
 
 function BootstrapDialogTitle(props) {
   const { children, onClose, ...other } = props;
@@ -64,62 +54,25 @@ export default function NoticeBoardModal() {
   };
 
   return (
-    <>
-      <motion.div
-      >
-        <BootstrapDialog
-          onClose={handleClose}
-          aria-labelledby="customized-dialog-title"
-          open={open}
-          fullScreen={fullScreen}
-          TransitionComponent={Transition}
-        >
-          <BootstrapDialogTitle
-            id="customized-dialog-title"
-            onClose={handleClose}
-          >
-            Notice Board
-          </BootstrapDialogTitle>
-          <DialogContent dividers>
-            <Typography gutterBottom>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-              ac consectetur ac, vestibulum at eros.
-            </Typography>
-            <Typography gutterBottom>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur
-              et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-              auctor.
-            </Typography>
-            <Typography gutterBottom>
-              Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-              cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-              dui. Donec ullamcorper nulla non metus auctor fringilla.
-            </Typography>
-
-            <Typography gutterBottom>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-              ac consectetur ac, vestibulum at eros.
-            </Typography>
-            <Typography gutterBottom>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur
-              et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-              auctor.
-            </Typography>
-            <Typography gutterBottom>
-              Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-              cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-              dui. Donec ullamcorper nulla non metus auctor fringilla.
-            </Typography>
-          </DialogContent>
-          <DialogActions>
-            <Button autoFocus onClick={handleClose}>
-              Close
-            </Button>
-          </DialogActions>
-        </BootstrapDialog>
-      </motion.div>
-    </>
+    <Dialog
+      onClose={handleClose}
+      aria-labelledby="customized-dialog-title"
+      open={open}
+      fullScreen={fullScreen}
+      TransitionComponent={Transition}
+      fullWidth
+    >
+      <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+        Notice Board
+      </BootstrapDialogTitle>
+      <DialogContent dividers>
+        <WindowCountDown />
+      </DialogContent>
+      <DialogActions>
+        <Button variant="contained" onClick={handleClose}>
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }

9bad1ca71c474b4555e7166ca769351da4803302
Kamzen <kamzen1994@gmail.com>
2023-09-18 15:09:53 +0200

    Update JobApplyModal.jsx

--- src/components/Modals/JobApplyModal.jsx
+++ src/components/Modals/JobApplyModal.jsx
@@ -2,25 +2,25 @@ import {
   Box,
   Button,
   Dialog,
-  DialogActions,
   DialogContent,
   DialogTitle,
   Grid,
-  InputLabel,
-  TextField
-  // useMediaQuery,
+  Stack,
+  TextField,
+  useMediaQuery,
+  useTheme
 } from "@mui/material";
-import { Form, Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 import React from "react";
 import TextFieldWrapper from "../FormComponents/TextFieldWrapper";
 import SelectFieldWrapper from "../FormComponents/SelectFieldWrapper";
-// import { useTheme } from "styled-components";
+import * as Yup from "yup";
 
-const JobApplyModal = () => {
+const JobApplyModal = ({ position }) => {
   const [open, setOpen] = React.useState(false);
 
-  // const theme = useTheme();
-  // const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -41,6 +41,8 @@ const JobApplyModal = () => {
     }
   ];
 
+  console.log(position);
+
   return (
     <Box>
       <Button variant="contained" onClick={handleClickOpen} fullWidth>
@@ -49,28 +51,57 @@ const JobApplyModal = () => {
       <Dialog
         open={open}
         onClose={handleClose}
-        // fullScreen={fullScreen}
+        fullScreen={fullScreen}
         fullWidth
         sx={{ width: "100%" }}
       >
-        <DialogTitle>Apply Job Title</DialogTitle>
+        <DialogTitle>{position.jobTitle}</DialogTitle>
         <DialogContent>
           <Formik
             initialValues={{
-              firstname: "",
+              positionId: position.id || "",
               fullname: "",
+              email: "",
               nationality: "",
               idNumber: "",
               gender: "",
-              cellphone: ""
+              cellphone: "",
+              resumeDocumentName: null,
+              idDocumentName: null,
+              matricDocumentName: null,
+              qualificationDocumentName: null
             }}
+            validationSchema={Yup.object().shape({
+              fullname: Yup.string().required("Fullname required"),
+              email: Yup.string().required("Email required"),
+              nationality: Yup.string().required("Nationality required"),
+              idNumber: Yup.string().required("ID number required"),
+              gender: Yup.string().required("Gender required"),
+              cellphone: Yup.string().required("Cellphone required"),
+              resumeDocumentName: Yup.string().required(
+                "Please upload your resume"
+              ),
+              idDocumentName: Yup.string().required(
+                "Please upload your id/passport"
+              ),
+              matricDocumentName: Yup.string().required(
+                "Please upload your matric certficate"
+              ),
+              qualificationDocumentName: Yup.string().required(
+                "Please upload your qualification"
+              )
+            })}
+            onSubmit={(values) => {}}
           >
-            {(values) => {
+            {({ values, setFieldValue }) => {
               return (
                 <Form>
                   <Grid container spacing={2}>
                     <Grid item xs={12} md={12} mt={2}>
                       <TextFieldWrapper name="fullname" label="Fullname" />
+                    </Grid>
+                    <Grid item xs={12} md={12} mt={2}>
+                      <TextFieldWrapper name="email" label="Email" />
                     </Grid>
                     <Grid item xs={12} md={12}>
                       <TextFieldWrapper
@@ -92,12 +123,136 @@ const JobApplyModal = () => {
                       <TextFieldWrapper name="cellphone" label="Cellphone" />
                     </Grid>
                     <Grid item xs={12} md={12}>
-                      <InputLabel>Id copy</InputLabel>
-                      <TextField name="idCopy" type="file" fullWidth />
+                      <Field name="idDocumentName">
+                        {({ field, form, meta }) => (
+                          <TextField
+                            type="file"
+                            label="Upload ID"
+                            InputLabelProps={{
+                              shrink: true
+                            }}
+                            error={meta.touched && meta.error}
+                            helperText={
+                              meta.touched && meta.error && meta.error
+                            }
+                            fullWidth
+                            onChange={(event) => {
+                              form.setFieldValue(
+                                field.name,
+                                event.currentTarget.files[0]
+                              );
+                            }}
+                          />
+                        )}
+                      </Field>
                     </Grid>
                     <Grid item xs={12} md={12}>
-                      <InputLabel>Resume</InputLabel>
-                      <TextField name="resume" type="file" fullWidth />
+                      <Field name="resumeDocumentName">
+                        {({ field, form, meta }) => (
+                          <TextField
+                            type="file"
+                            label="Upload Resume"
+                            InputLabelProps={{
+                              shrink: true
+                            }}
+                            error={meta.touched && meta.error}
+                            helperText={
+                              meta.touched && meta.error && meta.error
+                            }
+                            fullWidth
+                            onChange={(event) => {
+                              form.setFieldValue(
+                                field.name,
+                                event.currentTarget.files[0]
+                              );
+                            }}
+                          />
+                        )}
+                      </Field>
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                      <Field name="matricDocumentName">
+                        {({ field, form, meta }) => (
+                          <TextField
+                            type="file"
+                            label="Upload Matric"
+                            InputLabelProps={{
+                              shrink: true
+                            }}
+                            error={meta.touched && meta.error}
+                            helperText={
+                              meta.touched && meta.error && meta.error
+                            }
+                            fullWidth
+                            onChange={(event) => {
+                              form.setFieldValue(
+                                field.name,
+                                event.currentTarget.files[0]
+                              );
+                            }}
+                          />
+                        )}
+                      </Field>
+                    </Grid>
+
+                    {position?.PositionQualifications?.length > 0 && (
+                      <Grid item xs={12} md={12}>
+                        <Field name="qualificationDocumentName">
+                          {({ field, form, meta }) => (
+                            <TextField
+                              type="file"
+                              label="Upload Qualification"
+                              InputLabelProps={{
+                                shrink: true
+                              }}
+                              error={meta.touched && meta.error}
+                              helperText={
+                                meta.touched && meta.error && meta.error
+                              }
+                              fullWidth
+                              onChange={(event) => {
+                                form.setFieldValue(
+                                  field.name,
+                                  event.currentTarget.files[0]
+                                );
+                              }}
+                            />
+                          )}
+                        </Field>
+                      </Grid>
+                    )}
+                    {/* {position?.PositionQuestions?.length > 0 &&
+                      position?.PositionQuestions?.map(
+                        (positionQuestion, i) => {
+                          return (
+                            <Grid item xs={12} md={12}>
+                              <Typography sx={{ fontSize: 12, mb: 1 }}>
+                                {positionQuestion.question} ?
+                              </Typography>
+                              <TextField
+                                name={`question${i}`}
+                                label="Answer"
+                                fullWidth
+                                required
+                              />
+                            </Grid>
+                          );
+                        }
+                      )} */}
+
+                    <Grid item xs={12} md={12}>
+                      <Stack spacing={2} direction="row" justifyContent='end'>
+                        <Button
+                          onClick={handleClose}
+                          color="error"
+                          variant="contained"
+                        >
+                          Cancel
+                        </Button>
+                        <Button type="submit" variant="contained">
+                          Submit Application
+                        </Button>
+                      </Stack>
                     </Grid>
                   </Grid>
                 </Form>
@@ -105,14 +260,14 @@ const JobApplyModal = () => {
             }}
           </Formik>
         </DialogContent>
-        <DialogActions sx={{ px: 3 }}>
+        {/* <DialogActions sx={{ px: 3 }}>
           <Button onClick={handleClose} color="error" variant="contained">
             Cancel
           </Button>
-          <Button onClick={handleClose} autoFocus variant="contained">
-            Submit
+          <Button type="submit" variant="contained">
+            Submit Application
           </Button>
-        </DialogActions>
+        </DialogActions> */}
       </Dialog>
     </Box>
   );

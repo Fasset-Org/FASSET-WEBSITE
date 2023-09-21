acf3d830cf32d00859891db326382e4e23be8fa8
Kamzen <kamzen1994@gmail.com>
2023-09-21 14:29:42 +0200

    Update axiosInstance.js

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/dev";
+const BASE_URL = "102.37.217.58:5000/api/dev";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,

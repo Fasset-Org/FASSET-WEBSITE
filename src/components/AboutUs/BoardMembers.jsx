4ea9cba55fd926ac392ae59badc5da694821fa26
Kamzen <kamzen1994@gmail.com>
2024-01-31 09:20:26 +0200

    Update BoardMembers.jsx

--- src/components/AboutUs/BoardMembers.jsx
+++ src/components/AboutUs/BoardMembers.jsx
@@ -1,94 +1,32 @@
-import { Grid, Stack, Typography } from "@mui/material";
+import { Grid, Skeleton, Stack, Typography } from "@mui/material";
 import React from "react";
 import ProfileCard from "./ProfileCard";
-import Mathibela from "../../images/board/Bongani Mathibela-Organised Business.JPG";
-import Minister from "../../images/board/Minister of Higher Education, Science and Technology Dr Blade Nzimande NGI_8470.jpg";
-import James from "../../images/board/Mr James Maboa.JPG";
-import John from "../../images/board/Mr John Raphela.JPG";
-import Mbasa from "../../images/board/Mr Mbasa Metuse-Organised Labour .jpg";
-import Khumalo from "../../images/board/Mr Peterson  Khumalo.JPG";
-import Xola from "../../images/board/Mr Xola Lingani.JPG";
-import Angela from "../../images/board/Ms Angela Nyathela-Mahanjana-Organised Business .JPG";
-import Nhlapo from "../../images/board/Ms Mamatshiliso Nhlapo-Government Department.JPG";
-import Mopo from "../../images/board/Ms Mopo Mushwana-Organised Business.JPG";
-import Pumla from "../../images/board/Ms Pumla Ngwenya-Organised Business.JPG";
-import Rofhiwa from "../../images/board/Ms Rofhiwa Singo-Community Organisation.JPG";
-import Thnandi from "../../images/board/Ms Thandi Masemola-Organised Labour.JPG";
-import Tlakale from "../../images/board/Ms Tlakale Phatlane-Organised Labour .JPG";
+import { useQuery } from "@tanstack/react-query";
+import UserQuery from "../../stateQueries/User";
 
 const BoardMembers = () => {
-  const boardMembers = [
-    {
-      name: "Mr Bongani Mathibela",
-      position: "Organised Bussiness",
-      imgSrc: Mathibela
-    },
-    {
-      name: "Mr Blade Nzimande",
-      position: "Minister",
-      imgSrc: Minister
-    },
-    {
-      name: "Mr James Maboa",
-      position: "Organised Labour",
-      imgSrc: James
-    },
-    {
-      name: "Mr John Raphela",
-      position: "Organised Labour",
-      imgSrc: John
-    },
-    {
-      name: "Mr Mbasa Metuse",
-      position: "Organised Labour",
-      imgSrc: Mbasa
-    },
-    {
-      name: "Mr Peterson  Khumalo",
-      position: "Organised Labour",
-      imgSrc: Khumalo
-    },
-    {
-      name: "Mr Xola Lingani",
-      position: "Organised Labour",
-      imgSrc: Xola
-    },
-    {
-      name: "Ms Angela Nyathela-Mahanjana",
-      position: "Organised Business",
-      imgSrc: Angela
-    },
-    {
-      name: "Ms Mamatshiliso Nhlapo",
-      position: "Government Department",
-      imgSrc: Nhlapo
-    },
-    {
-      name: "Ms Mopo Mushwana",
-      position: "Organised Business",
-      imgSrc: Mopo
-    },
-    {
-      name: "Ms Pumla Ngwenya",
-      position: "Organised Business",
-      imgSrc: Pumla
-    },
-    {
-      name: "Ms Rofhiwa Singo",
-      position: "Community Organisation",
-      imgSrc: Rofhiwa
-    },
-    {
-      name: "Ms Thandi Masemola",
-      position: "Organised Labour",
-      imgSrc: Thnandi
-    },
-    {
-      name: "Ms Tlakale Phatlane",
-      position: "Organised Labour",
-      imgSrc: Tlakale
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["boardMembers"],
+    queryFn: async () => {
+      return UserQuery.getAllBoardMembers();
     }
-  ];
+  });
+
+  if (isLoading || error) {
+    return (
+      <div>
+        <Grid container spacing={2}>
+          {[...Array(5)].map((skeleton) => {
+            return (
+              <Grid item xs={12} md={4}>
+                <Skeleton variant="rectangular" width={"100%"} height={300} />;
+              </Grid>
+            );
+          })}
+        </Grid>
+      </div>
+    );
+  }
 
   return (
     <Stack>
@@ -101,13 +39,14 @@ const BoardMembers = () => {
       </Typography>
 
       <Grid container spacing={2}>
-        {boardMembers.map((member) => {
+        {data?.boardMembers.map((member) => {
           return (
-            <Grid item xs={12} md={4}>
+            <Grid item xs={12} md={4} key={member.id}>
               <ProfileCard
-                name={member.name}
+                title={member.title}
+                fullName={member.fullname}
                 position={member.position}
-                imgSrc={member.imgSrc}
+                imageFileURL={member.imageFileURL}
               />
             </Grid>
           );

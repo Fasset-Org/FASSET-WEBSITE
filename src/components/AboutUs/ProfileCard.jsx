e3fceff4cce91aea57f931f03495a93c8dc2289a
Kamzen <kamzen1994@gmail.com>
2024-01-31 09:20:18 +0200

    Update ProfileCard.jsx

--- src/components/AboutUs/ProfileCard.jsx
+++ src/components/AboutUs/ProfileCard.jsx
@@ -1,11 +1,11 @@
 import { Paper, Stack, Typography } from "@mui/material";
 import React from "react";
 
-const ProfileCard = ({name, position, imgSrc}) => {
+const ProfileCard = ({ title, fullName, position, imageFileURL }) => {
   return (
     <Stack component={Paper} height={300} boxShadow={5} borderRadius={3}>
       <img
-        src={imgSrc}
+        src={`http://localhost:8001/uploads/board-members/${imageFileURL}`}
         alt=""
         width="100%"
         height="70%"
@@ -15,6 +15,7 @@ const ProfileCard = ({name, position, imgSrc}) => {
           borderRadius: 10
         }}
       />
+
       <Stack
         padding={1}
         component={Paper}
@@ -31,12 +32,10 @@ const ProfileCard = ({name, position, imgSrc}) => {
             color: "primary.main"
           }}
         >
-          {position}
+          {position.toUpperCase()}
         </Typography>
-        <Typography
-          sx={{ fontWeight: "bolder",  fontSize: 15 }}
-        >
-          {name}
+        <Typography sx={{ fontWeight: "bolder", fontSize: 15 }}>
+          {title} {fullName}
         </Typography>
       </Stack>
     </Stack>

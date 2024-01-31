c30001d2e24760fd76f33c85d6a111d07c112594
Kamzen <kamzen1994@gmail.com>
2024-01-31 09:20:05 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
 import LatestNews from "../components/Latest News/LatestNews";
-// import banner1 from "../images/bgImage1.avif";
-import banner2 from "../images/Website-Deadline-Extension.png";
-import banner3 from "../images/sigmund-LCun3uxh-z0-unsplash.jpg";
-import banner4 from "../images/gallery-images/Nkandla.jpg";
 import Slideshow from "../components/SlideShow";
 import ValueCard from "../components/ValueCard";
 import FassetAssistant from "../components/FassetAssistant";
 import NoticeBoardModal from "../components/NoticeBoardModal";
 
 const Home = ({ id }) => {
-  const images2 = [banner2, banner3, banner4];
-
   const theme = useTheme();
   const xs = useMediaQuery(theme.breakpoints.down("xs"));
   const sm = useMediaQuery(theme.breakpoints.down("sm"));
@@ -24,7 +18,7 @@ const Home = ({ id }) => {
 
       {xs || sm || md ? (
         <>
-          <Slideshow images={images2} height="300px" />
+          <Slideshow />
           <NoticeBoardModal />
           <FassetAssistant />
 
@@ -38,11 +32,7 @@ const Home = ({ id }) => {
             Values
           </Typography>
 
-          <Stack
-            direction='column'
-            spacing={2}
-            padding={2}
-          >
+          <Stack direction="column" spacing={2} padding={2}>
             <ValueCard
               title="Professionalism & Accountability"
               definition={
@@ -74,7 +64,7 @@ const Home = ({ id }) => {
         </>
       ) : (
         <>
-          <Slideshow images={images2} height="400px" />
+          <Slideshow />
 
           <NoticeBoardModal />
 
@@ -92,11 +82,7 @@ const Home = ({ id }) => {
             Values
           </Typography>
 
-          <Stack
-            direction='row'
-            spacing={2}
-            padding={2}
-          >
+          <Stack direction="row" spacing={2} padding={2}>
             <ValueCard
               title="Professionalism And Accountability"
               definition={

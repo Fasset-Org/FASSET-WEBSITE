a06c21fae475ec417415fbcc944e89506d9416a0
Kamzen <kamzen1994@gmail.com>
2024-01-31 09:20:23 +0200

    Update axiosInstance.js

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/dev",
+  baseURL: "http://localhost:8001/api/dev",
   // baseURL: "http://102.37.217.58:5000/api/dev",
   withCredentials: true
 });

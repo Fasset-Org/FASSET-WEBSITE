048a038b59dddf45932436a7f5c2f5b09ba31964
SiyamthandaNyathi <131876850+SiyamthandaNyathi@users.noreply.github.com>
2024-02-08 10:31:58 +0200

    Update

--- src/pages/Resources/Resources.jsx
+++ src/pages/Resources/Resources.jsx
@@ -117,66 +117,7 @@ const Resources = () => {
       </TabPanel>
       <TabPanel value={value} index={2}>
         <AnnualReports />
-        <Typography>Fasset's Annual Integrated Report for the period 1 April 2021 to 31 March 2022 is available for download.
-Hard copies are also available from Thabiso Sebashe at Fasset on 011 476 8570.</Typography>
-<br/>
-<Typography>Click below to download an electronic version. Note: This file is fairly large and may take a while to download.</Typography><br/>
-<br/>
-<br/>
-<Typography>1 April 2022 – 31 March 2023</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2022</Typography>
-<br/>
-<Typography>1 April 2020 to 31 March 2021</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2020</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2019</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2018</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2017</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2016</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2015</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2014</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2013</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2012</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2011</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2010</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2009</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2008</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2007</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2006</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2005</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2006</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2004</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2003</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2002</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2001</Typography>
-<br/>
-<Typography>1 April 2021 – 31 March 2000</Typography>
-
-
-
-<Typography>Fasset Five Year Overview</Typography>
-<Typography>The Fasset Five Year Overview was included as a separate insert to the Year 5 annual report. It provides a wonderful snapshot of Fasset's achievements in terms of the first five year NSDS and we are proud to report that we have excelled in all areas.</Typography>
+       
       </TabPanel>
     </Box>
   );

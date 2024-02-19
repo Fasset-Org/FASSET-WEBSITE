ca15692b0c310ead2b397005cdacd31f583a3a85
Kamzen <kamzen1994@gmail.com>
2024-02-19 14:18:00 +0200

    Update LatestNews.jsx

--- src/components/Latest News/LatestNews.jsx
+++ src/components/Latest News/LatestNews.jsx
@@ -1,12 +1,136 @@
-import { Stack } from '@mui/material'
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Masonry from "@mui/lab/Masonry";
+import { styled } from "@mui/material/styles";
+import { Stack, Typography } from "@mui/material";
+// import img1 from '../../images/AGM/ZooM-6738.jpg'
+// import img2 from '../../images/AGM/ZooM-6753.jpg'
+// import img3 from '../../images/AGM/ZooM-6835.jpg'
+// import img4 from '../../images/AGM/ZooM-6852.jpg'
+// import img5 from '../../images/AGM/ZooM-6872.jpg'
+// import img6 from '../../images/AGM/ZooM-6877.jpg'
+// import img7 from '../../images/AGM/ZooM-6884.jpg'
+// import img8 from '../../images/AGM/ZooM-6898.jpg'
+// import img9 from '../../images/AGM/ZooM-6930.jpg'
+// import img10 from '../../images/AGM/ZooM-6931.jpg'
+// import img11 from '../../images/AGM/ZooM-6939.jpg'
+// import img12 from '../../images/AGM/ZooM-6738.jpg'
+// import img13 from '../../images/AGM/ZooM-6738.jpg'
+// import img14 from '../../images/AGM/ZooM-6738.jpg'
 
-const LatestNews = () => {
+const Label = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(0.5),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  borderBottomLeftRadius: 0,
+  borderBottomRightRadius: 0
+}));
+
+export default function LatestNews() {
   return (
-    <Stack height={300} border={1} borderColor='lightgray'>
-
+    <Stack spacing={2} padding={2}>
+      <Typography
+        fontWeight="bolder"
+        fontSize={20}
+        sx={{ color: "primary.main", textTransform: "uppercase", mb: 2, textAlign: 'center' }}
+      >
+        GALLERY
+      </Typography>
+      <Box sx={{ width: "100%", minHeight: 829, paddingX: 10 }}>
+        <Masonry columns={3} spacing={2}>
+          {itemData.map((item, index) => (
+            <div key={index}>
+              <Label>{index + 1}</Label>
+              <img
+                srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
+                src={`${item.img}?w=162&auto=format`}
+                alt={item.title}
+                loading="lazy"
+                style={{
+                  borderBottomLeftRadius: 4,
+                  borderBottomRightRadius: 4,
+                  display: "block",
+                  width: "100%"
+                }}
+              />
+            </div>
+          ))}
+        </Masonry>
+      </Box>
     </Stack>
-  )
+  );
 }
 
-export default LatestNews
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1627308595229-7830a5c91f9f",
+    title: "Snacks"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1529655683826-aba9b3e77383",
+    title: "Tower"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1627328715728-7bcc1b5db87d",
+    title: "Tree"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1627000086207-76eabf23aa2e",
+    title: "Camping Car"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1627328561499-a3584d4ee4f7",
+    title: "Mountain"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike"
+  }
+];

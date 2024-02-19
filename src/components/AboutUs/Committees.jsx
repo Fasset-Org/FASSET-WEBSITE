dbb3d11ec3e97d9e3d5f3fcba543859be513df5a
Kamzen <kamzen1994@gmail.com>
2024-02-19 14:00:41 +0200

    Update Committees.jsx

--- src/components/AboutUs/Committees.jsx
+++ src/components/AboutUs/Committees.jsx
@@ -1,10 +1,18 @@
-import { Accordion, AccordionDetails, AccordionSummary, Grid, Skeleton, Stack, Typography } from "@mui/material";
+import {
+  // Accordion,
+  // AccordionDetails,
+  // AccordionSummary,
+  Grid,
+  Skeleton,
+  Stack,
+  Typography
+} from "@mui/material";
 import React from "react";
 import ProfileCard from "./ProfileCard";
 import { useQuery } from "@tanstack/react-query";
 import UserQuery from "../../stateQueries/User";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import SectorSkillPlan from "../Resources/ResearchComponents/SectorSkillPlan";
+// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+// import SectorSkillPlan from "../Resources/ResearchComponents/SectorSkillPlan";
 
 const Committees = () => {
   const { data, isLoading, error } = useQuery({
@@ -31,55 +39,54 @@ const Committees = () => {
   }
 
   return (
-
-    <Stack spacing={1}>
-      <Typography
-        fontWeight="bolder"
-        fontSize={20}
-        sx={{ color: "primary.main", textTransform: "uppercase" }}
-      >
-        Research
-      </Typography>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon sx={{ color: "#FFFFFF" }} />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-          sx={{ backgroundColor: "primary.main" }}
-        >
-          <Typography>Sector Skills Plan</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            <SectorSkillPlan />
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    </Stack>
-    // <Stack>
+    // <Stack spacing={1}>
     //   <Typography
     //     fontWeight="bolder"
     //     fontSize={20}
-    //     sx={{ color: "primary.main", textTransform: "uppercase", mb: 2 }}
+    //     sx={{ color: "primary.main", textTransform: "uppercase" }}
     //   >
-    //     Executive Members
+    //     Research
     //   </Typography>
-
-    //   <Grid container spacing={2}>
-    //     {data?.committeeMembers?.map((member) => {
-    //       return (
-    //         <Grid item xs={12} md={4} key={member.id}>
-    //           <ProfileCard
-    //             title={member.title}
-    //             fullName={member.fullname}
-    //             position={member.position}
-    //             imageFileURL={member.imageFileURL}
-    //           />
-    //         </Grid>
-    //       );
-    //     })}
-    //   </Grid>
+    //   <Accordion>
+    //     <AccordionSummary
+    //       expandIcon={<ExpandMoreIcon sx={{ color: "#FFFFFF" }} />}
+    //       aria-controls="panel1a-content"
+    //       id="panel1a-header"
+    //       sx={{ backgroundColor: "primary.main" }}
+    //     >
+    //       <Typography>Sector Skills Plan</Typography>
+    //     </AccordionSummary>
+    //     <AccordionDetails>
+    //       <Typography>
+    //         <SectorSkillPlan />
+    //       </Typography>
+    //     </AccordionDetails>
+    //   </Accordion>
     // </Stack>
+    <Stack>
+      <Typography
+        fontWeight="bolder"
+        fontSize={20}
+        sx={{ color: "primary.main", textTransform: "uppercase", mb: 2 }}
+      >
+        Executive Members
+      </Typography>
+
+      <Grid container spacing={2}>
+        {data?.committeeMembers?.map((member) => {
+          return (
+            <Grid item xs={12} md={4} key={member.id}>
+              <ProfileCard
+                title={member.title}
+                fullName={member.fullname}
+                position={member.position}
+                imageFileURL={member.imageFileURL}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Stack>
   );
 };
 

2571d63a00be5520994294259897eb8616739f48
Kamzen <kamzen1994@gmail.com>
2024-02-19 14:23:07 +0200

    Update PreviousVacancies.jsx

--- src/components/Vacancies/PreviousVacancies.jsx
+++ src/components/Vacancies/PreviousVacancies.jsx
@@ -19,7 +19,6 @@ const PreviousVacancies = () => {
   return (
     <Stack>
       <Typography
-        fontFamily="Helvetica Neue"
         fontWeight="bolder"
         fontSize={20}
         sx={{ color: "primary.main", textTransform: "uppercase", mb: 2 }}

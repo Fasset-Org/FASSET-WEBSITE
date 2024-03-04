b7c1fa67e73d4e3ff0d77414de5d1a17423a40e4
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-03-04 11:22:59 +0200

    Update axiosInstance.js

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8001/api/dev",
-  // baseURL: "http://102.37.217.58:5000/api/dev",
+  // baseURL: "http://localhost:8001/api/dev",
+  baseURL: "http://102.37.217.58:5000/api/dev",
   withCredentials: true
 });
 

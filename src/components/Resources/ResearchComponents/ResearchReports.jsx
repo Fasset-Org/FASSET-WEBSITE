9027d938e2f3ecfae5e667f1991abb65dc091d11
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-03-05 04:20:56 +0200

    Update ResearchReports.jsx

--- src/components/Resources/ResearchComponents/ResearchReports.jsx
+++ src/components/Resources/ResearchComponents/ResearchReports.jsx
@@ -165,7 +165,7 @@ const ResearchReports = () => {
                         >
                           <MenuItem>
                             <a
-                              href={`http://localhost:8001/uploads/board-members/${row.researchReportFileURL}`}
+                              href={`http://102.37.217.58:5000/uploads/board-members/${row.researchReportFileURL}`}
                               target="_blank"
                               rel="noreferrer"
                               style={{ all: "unset" }}
@@ -175,7 +175,7 @@ const ResearchReports = () => {
                           </MenuItem>
                           <MenuItem>
                             <a
-                              href={`http://localhost:8001/uploads/board-members/${row.researchReportFileURL}`}
+                              href={`http://102.37.217.58:5000/uploads/board-members/${row.researchReportFileURL}`}
                               target="_blank"
                               rel="noreferrer"
                               style={{ all: "unset" }}

f849d8fa719e284cb98b09732f080cde542d77cb
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-05-23 13:49:09 +0200

    Update EISA.jsx

--- src/components/QualityAssurance/EISA.jsx
+++ src/components/QualityAssurance/EISA.jsx
@@ -19,14 +19,11 @@ const EISA = () => {
         will issue the learners with Statement of Results (SoR). The SoRs should
         reflect achievement by learners against the three components of an
         occupational qualification, i.e., Knowledge, Practical Experience, and
-        Workplace Experience.
-      </Typography>
-
-      <Typography textAlign="justify">
-        EISA’s are conducted through accredited Assessment Centres. Registration
-        for EISA can be requested by the SDP or by the individual leaner. FASSET
-        will publish a calendar or dates for the scheduling of EISAs for all the
-        occupational qualifications with learners in the system.
+        Workplace Experience. EISA’s are conducted through accredited Assessment
+        Centres. Registration for EISA can be requested by the SDP or by the
+        individual leaner. FASSET will publish a calendar or dates for the
+        scheduling of EISAs for all the occupational qualifications with
+        learners in the system.
       </Typography>
     </Stack>
   );

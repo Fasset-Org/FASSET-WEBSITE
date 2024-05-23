c303de7bc66db13d3163826c8f609ff5a68e3a47
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-05-23 11:09:40 +0200

    Create ProviderAccreditation.jsx

---
+++ src/components/QualityAssurance/ProviderAccreditation.jsx
@@ -0,0 +1,27 @@
+import { Stack, Typography } from "@mui/material";
+import React from "react";
+
+const ProviderAccreditation = () => {
+  return (
+    <Stack spacing={2}>
+      <Typography
+        fontSize={20}
+        fontWeight="bolder"
+        sx={{ color: "primary.main" }}
+      >
+        Provider Accreditation
+      </Typography>
+      <Typography textAlign="justify">
+        Kindly be informed that as from 1 July 2022, Fasset will no longer
+        accredit or re-accredit skills development providers on historical
+        qualification expiring in June 2023. All Fasset qualifications are in
+        the process of realignment with occupational qualifications. We
+        apologise for thge inconvenience this may have caused to our current and
+        new skilld development providers. Please be on the lookout for further
+        announcement relating to this.
+      </Typography>
+    </Stack>
+  );
+};
+
+export default ProviderAccreditation;

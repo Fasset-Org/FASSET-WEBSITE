24f2d8b204b1568447e797865c3291c063062b03
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-05-31 07:56:38 +0200

    Update PreviousVacancies.jsx

--- src/components/Vacancies/PreviousVacancies.jsx
+++ src/components/Vacancies/PreviousVacancies.jsx
@@ -1,10 +1,42 @@
-import { Grid, LinearProgress, Stack, Typography } from "@mui/material";
+import {
+  Alert,
+  Grid,
+  IconButton,
+  LinearProgress,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography
+} from "@mui/material";
 import React from "react";
-import VacancyCard from "../../components/VacancyCard";
 import { useQuery } from "@tanstack/react-query";
 import UserQuery from "../../stateQueries/User";
+import TablePaginationActions from "@mui/material/TablePagination/TablePaginationActions";
+import { useNavigate } from "react-router-dom";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 
 const PreviousVacancies = () => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const navigate = useNavigate();
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   const { data, isLoading } = useQuery({
     queryKey: ["previousPositions"],
     queryFn: async () => {
@@ -17,18 +49,130 @@ const PreviousVacancies = () => {
   }
 
   return (
-    <Stack>
+    <Stack spacing={2}>
       <Typography
         fontWeight="bolder"
         fontSize={20}
-        sx={{ color: "primary.main", textTransform: "uppercase", mb: 2 }}
+        sx={{ color: "primary.main", textTransform: "uppercase", }}
       >
         Previous Vacancies
       </Typography>
       <Grid container spacing={2}>
-        {data?.positions?.map((vacancy, i) => {
-          return <VacancyCard vacancy={vacancy} />;
-        })}
+        {data?.positions?.length > 0 ? (
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead sx={{ backgroundColor: "background.paper" }}>
+                <TableRow
+                  sx={{ backgroundColor: "primary.main", color: "#FFFFFF" }}
+                >
+                  <TableCell sx={{ fontWeight: "bolder", color: "#FFFFFF" }}>
+                    No#
+                  </TableCell>
+                  <TableCell sx={{ fontWeight: "bolder", color: "#FFFFFF" }}>
+                    Job Title
+                  </TableCell>
+                  <TableCell sx={{ fontWeight: "bolder", color: "#FFFFFF" }}>
+                    Department
+                  </TableCell>
+                  <TableCell sx={{ fontWeight: "bolder", color: "#FFFFFF" }}>
+                    Line Manager
+                  </TableCell>
+                  <TableCell sx={{ fontWeight: "bolder", color: "#FFFFFF" }}>
+                    Remuneration
+                  </TableCell>
+                  <TableCell sx={{ fontWeight: "bolder", color: "#FFFFFF" }}>
+                    Date Uploaded
+                  </TableCell>
+                  <TableCell sx={{ fontWeight: "bolder", color: "#FFFFFF" }}>
+                    Closing Date
+                  </TableCell>
+                  <TableCell sx={{ fontWeight: "bolder", color: "#FFFFFF" }}>
+                    Actions
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {(rowsPerPage > 0
+                  ? data?.positions?.slice(
+                      page * rowsPerPage,
+                      page * rowsPerPage + rowsPerPage
+                    )
+                  : data?.positions
+                )?.map((position, i) => {
+                  return (
+                    <TableRow key={position.id}>
+                      <TableCell component="th" scope="row">
+                        {i + 1}
+                      </TableCell>
+                      <TableCell component="th" scope="row">
+                        {position.jobTitle}
+                      </TableCell>
+
+                      <TableCell component="th" scope="row">
+                        {position?.Department?.departmentName}
+                      </TableCell>
+
+                      <TableCell component="th" scope="row">
+                        {position.reportingTo}
+                      </TableCell>
+
+                      <TableCell component="th" scope="row">
+                        {position.remuneration}
+                      </TableCell>
+                      <TableCell component="th" scope="row">
+                        {`${new Date(position.createdAt).toDateString()}`}
+                      </TableCell>
+                      <TableCell component="th" scope="row">
+                        {`${new Date(position.closingDate).toDateString()}`}
+                      </TableCell>
+
+                      <TableCell>
+                        <IconButton
+                          onClick={() => {
+                            navigate(`/vacancies/${position.id}`);
+                          }}
+                        >
+                          <VisibilityIcon />
+                        </IconButton>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+              {data?.positions?.length > 0 && (
+                <TableFooter>
+                  <TableRow>
+                    <TablePagination
+                      rowsPerPageOptions={[
+                        5,
+                        10,
+                        25,
+                        { label: "All", value: -1 }
+                      ]}
+                      // colSpan={3}
+                      count={data?.positions?.length || 0}
+                      rowsPerPage={rowsPerPage}
+                      page={page}
+                      SelectProps={{
+                        inputProps: {
+                          "aria-label": "rows per page"
+                        },
+                        native: true
+                      }}
+                      onPageChange={handleChangePage}
+                      onRowsPerPageChange={handleChangeRowsPerPage}
+                      ActionsComponent={TablePaginationActions}
+                    />
+                  </TableRow>
+                </TableFooter>
+              )}
+            </Table>
+          </TableContainer>
+        ) : (
+          <Stack width="100%" spacing={2} ml={2}>
+            <Alert severity="info">No Positions Available</Alert>
+          </Stack>
+        )}
       </Grid>
     </Stack>
   );

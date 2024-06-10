0267c89a642036fcaed14ba887f4e1da64ab1ae8
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-06-10 13:32:25 +0200

    Create WorkplaceApprovalMobile.jsx

---
+++ src/pages/Mobile/QualityAssurance/WorkplaceApprovalMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import WorkplaceApproval from '../../../components/QualityAssurance/WorkplaceApproval'
+
+const WorkplaceApprovalMobile = () => {
+  return (
+    <Stack padding={2}>
+      <WorkplaceApproval />
+    </Stack>
+  )
+}
+
+export default WorkplaceApprovalMobile

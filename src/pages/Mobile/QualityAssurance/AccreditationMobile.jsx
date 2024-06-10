cca4933e645eda0d30c27251a70d4fd015a14c34
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-06-10 13:33:35 +0200

    Create AccreditationMobile.jsx

---
+++ src/pages/Mobile/QualityAssurance/AccreditationMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import Accreditation from '../../../components/QualityAssurance/Accreditation'
+
+const AccreditationMobile = () => {
+  return (
+    <Stack padding={2}>
+      <Accreditation />
+    </Stack>
+  )
+}
+
+export default AccreditationMobile

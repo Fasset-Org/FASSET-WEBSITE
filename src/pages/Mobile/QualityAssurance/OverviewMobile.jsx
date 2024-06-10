18f3f339562bde5ce2e2eb43b85dda3786d2548b
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-06-10 13:33:17 +0200

    Create OverviewMobile.jsx

---
+++ src/pages/Mobile/QualityAssurance/OverviewMobile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Overview from '../../../components/QualityAssurance/Overview'
+import { Stack } from '@mui/material'
+
+const OverviewMobile = () => {
+  return (
+    <Stack padding={2}>
+      <Overview />
+    </Stack>
+  )
+}
+
+export default OverviewMobile

74828775d5c588d3e3ad00b68040089a5e34eca8
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-06-10 13:33:23 +0200

    Create EMPMobile.jsx

---
+++ src/pages/Mobile/QualityAssurance/EMPMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import ExternalModerationProcess from '../../../components/QualityAssurance/ExternalModerationProcess'
+
+const EMPMobile = () => {
+  return (
+    <Stack padding={2}>
+      <ExternalModerationProcess />
+    </Stack>
+  )
+}
+
+export default EMPMobile

2e38ecce75de9cb07f8b6c0295c722ad37bf7c54
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-06-10 13:34:20 +0200

    Create QTCOPoliciesMobile.jsx

---
+++ src/pages/Mobile/QualityAssurance/QTCOPoliciesMobile.jsx
@@ -0,0 +1,12 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+
+const QTCOPoliciesMobile = () => {
+  return (
+    <Stack padding={2}>
+      <QTCOPoliciesMobile />
+    </Stack>
+  )
+}
+
+export default QTCOPoliciesMobile

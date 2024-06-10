b9d0885cde2e346735a1b61d6d72702a8f142d35
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-06-10 13:32:39 +0200

    Create ACAMobile.jsx

---
+++ src/pages/Mobile/QualityAssurance/ACAMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import AssessmentCentresAccreditation from '../../../components/QualityAssurance/AssessmentCentresAccreditation'
+
+const ACAMobile = () => {
+  return (
+    <Stack padding={2}>
+      <AssessmentCentresAccreditation />
+    </Stack>
+  )
+}
+
+export default ACAMobile

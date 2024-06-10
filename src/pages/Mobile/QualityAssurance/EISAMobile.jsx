0b3d906ee7aa93c5c0af22bc01af67449668213f
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-06-10 13:32:33 +0200

    Create EISAMobile.jsx

---
+++ src/pages/Mobile/QualityAssurance/EISAMobile.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import EISA from '../../../components/QualityAssurance/EISA'
+
+const EISAMobile = () => {
+  return (
+    <Stack padding={2}>
+      <EISA />
+    </Stack>
+  )
+}
+
+export default EISAMobile

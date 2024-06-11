2b3ded4010ed67db8147f2af18b2072d479e7c3a
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-06-11 14:49:28 +0200

    Update ChooseCareerMobile.jsx

--- src/pages/Mobile/Learners/ChooseCareerMobile.jsx
+++ src/pages/Mobile/Learners/ChooseCareerMobile.jsx
@@ -1,11 +1,13 @@
 import { Stack } from '@mui/material'
 import React from 'react'
 import ChooseACareer from '../../../components/Learners/ChooseACareer'
+import ScarceSkills from '../../../components/Learners/ScarceSkills'
 
 const ChooseCareerMobile = () => {
   return (
     <Stack padding={2}>
       <ChooseACareer />
+      <ScarceSkills />
     </Stack>
   )
 }

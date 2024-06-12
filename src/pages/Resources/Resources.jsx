48d02cdf78190b03c4efbeb2e1ea5e290a918540
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-06-12 10:41:08 +0200

    Update Resources.jsx

--- src/pages/Resources/Resources.jsx
+++ src/pages/Resources/Resources.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import PropTypes from "prop-types";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
@@ -10,6 +9,9 @@ import SummarizeIcon from "@mui/icons-material/Summarize";
 import Downloads from "../../components/Resources/Downloads";
 import Research from "../../components/Resources/Research";
 import AnnualReports from "../../components/Resources/AnnualReports";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
+import { Stack, useMediaQuery } from "@mui/material";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -32,12 +34,6 @@ function TabPanel(props) {
   );
 }
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired
-};
-
 function a11yProps(index) {
   return {
     id: `vertical-tab-${index}`,
@@ -46,80 +42,118 @@ function a11yProps(index) {
 }
 
 const Resources = () => {
-  const [value, setValue] = React.useState(0);
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
+  const navigate = useNavigate();
+
+  const { pathname } = useLocation();
+
+  const value = pathname.split("/")[1] || "downloads";
 
   const handleChange = (event, newValue) => {
-    setValue(newValue);
+    navigate(`/${newValue}`);
   };
 
-  return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        bgcolor: "background.paper",
-        display: "flex"
-      }}
-    >
-      <Tabs
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={handleChange}
-        textColor="secondary"
-        indicatorColor="secondary"
+  if (isDesktop) {
+    return (
+      <Box
         sx={{
-          borderRight: 1,
-          borderColor: "lightgray",
-          borderBottomRightRadius: 0,
-          backgroundColor: "primary.main",
-          minHeight: 450,
-          m: 2
-        }}
-        TabIndicatorProps={{
-          sx: {
-            border: 3,
-            borderColor: "secondary.main"
-          }
+          flexGrow: 1,
+          bgcolor: "background.paper",
+          display: "flex"
         }}
       >
-        <Tab
-          label="Downloads"
-          icon={<CloudDownloadIcon />}
-          iconPosition="start"
-          {...a11yProps(0)}
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={value}
+          onChange={handleChange}
+          textColor="secondary"
+          indicatorColor="secondary"
           sx={{
-            borderBottom: 1,
+            borderRight: 1,
             borderColor: "lightgray",
-            color: "#FFFFFF"
+            borderBottomRightRadius: 0,
+            backgroundColor: "primary.main",
+            minHeight: 450,
+            m: 2
           }}
-        />
-        <Tab
-          label="Research"
-          icon={<ScienceIcon />}
-          iconPosition="start"
-          {...a11yProps(1)}
-          sx={{ borderBottom: 1, borderColor: "lightgray", color: "#FFFFFF" }}
-        />
+          TabIndicatorProps={{
+            sx: {
+              border: 3,
+              borderColor: "secondary.main"
+            }
+          }}
+        >
+          <Tab
+            label="Downloads"
+            icon={<CloudDownloadIcon />}
+            iconPosition="start"
+            {...a11yProps("downloads")}
+            sx={{
+              borderBottom: 1,
+              borderColor: "lightgray",
+              color: "#FFFFFF"
+            }}
+            value="downloads"
+          />
+          <Tab
+            label="Research"
+            icon={<ScienceIcon />}
+            iconPosition="start"
+            {...a11yProps("research")}
+            sx={{ borderBottom: 1, borderColor: "lightgray", color: "#FFFFFF" }}
+            value="research"
+          />
 
-        <Tab
-          label="Annual Reports"
-          icon={<SummarizeIcon />}
-          iconPosition="start"
-          {...a11yProps(1)}
-          sx={{ borderBottom: 1, borderColor: "lightgray", color: "#FFFFFF" }}
-        />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        <Downloads />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <Research />
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <AnnualReports />
-      </TabPanel>
-    </Box>
-  );
+          <Tab
+            label="Annual Reports"
+            icon={<SummarizeIcon />}
+            iconPosition="start"
+            {...a11yProps("annualReports")}
+            sx={{ borderBottom: 1, borderColor: "lightgray", color: "#FFFFFF" }}
+            value="annualReports"
+          />
+        </Tabs>
+        <TabPanel value={value} index="downloads">
+          <Downloads />
+        </TabPanel>
+        <TabPanel value={value} index="research">
+          <Research />
+        </TabPanel>
+        <TabPanel value={value} index="annualReports">
+          <AnnualReports />
+        </TabPanel>
+      </Box>
+    );
+  } else {
+    switch (value) {
+      case "downloads":
+        return (
+          <Stack padding={2}>
+            <Downloads />
+          </Stack>
+        );
+      case "research":
+        return (
+          <Stack padding={2}>
+            <Research />
+          </Stack>
+        );
+      case "annualReports":
+        return (
+          <Stack padding={2}>
+            <AnnualReports />
+          </Stack>
+        );
+      default:
+        return (
+          <Stack padding={2}>
+            <Downloads />
+          </Stack>
+        );
+    }
+  }
 };
 
 export default Resources;

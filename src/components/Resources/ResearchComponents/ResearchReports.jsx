3477898c0604af3a27a0c9ab316fbd54b351d78e
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-06-12 12:49:09 +0200

    Update ResearchReports.jsx

--- src/components/Resources/ResearchComponents/ResearchReports.jsx
+++ src/components/Resources/ResearchComponents/ResearchReports.jsx
@@ -15,7 +15,7 @@ import {
   TableRow,
   Typography
 } from "@mui/material";
-import React from "react";
+import React, { Fragment } from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import TablePaginationActions from "@mui/material/TablePagination/TablePaginationActions";
 import { useQuery } from "@tanstack/react-query";
@@ -50,7 +50,7 @@ const ResearchReports = () => {
     setAnchorEl(null);
   };
 
-  console.log(data);
+  // console.log(data);
 
   return (
     <Stack spacing={1}>
@@ -70,7 +70,7 @@ const ResearchReports = () => {
       {isLoading ? (
         <LinearProgress />
       ) : (
-        <>
+        <Fragment>
           {data?.researchReports && (
             <TableContainer component={Paper} sx={{ borderRadius: 0 }}>
               <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -215,7 +215,7 @@ const ResearchReports = () => {
               </Table>
             </TableContainer>
           )}
-        </>
+        </Fragment>
       )}
     </Stack>
   );

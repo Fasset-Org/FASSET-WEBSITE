968950b2983c4ac2a7a2949005818805313fd52d
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-06-12 12:49:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,21 +28,15 @@ import ProfessionalBodies from "./pages/ProfessionalBodies/ProfessionalBodies";
 import TenderDetails from "./pages/SupplyChain/TenderDetails";
 import FallbackError from "./pages/FallbackError";
 import MainNavigation from "./components/Navigation/MainNavigation";
-import WhoWeAreMobile from "./pages/Mobile/AboutUs/WhoWeAreMobile";
-import OurMandateMobile from "./pages/Mobile/AboutUs/OurMandateMobile";
-import BoardMobile from "./pages/Mobile/AboutUs/BoardMobile";
-import CommitteesMobile from "./pages/Mobile/AboutUs/CommitteesMobile";
-import ConstitutionMobile from "./pages/Mobile/AboutUs/ConstitutionMobile";
-import PoliciesMobile from "./pages/Mobile/AboutUs/PoliciesMobile";
-import ChooseCareerMobile from "./pages/Mobile/Learners/ChooseCareerMobile";
-import BursaryMobile from "./pages/Mobile/Learners/BursaryMobile";
-import AboutFassetLearnershipsMobile from "./pages/Mobile/ProfessionalBodies/AboutFassetLearnershipsMobile";
-import InternshipsMobile from "./pages/Mobile/Learners/InternshipsMobile";
-
 
 function App() {
   const [theme, setTheme] = useState("light");
   const queryClient = new QueryClient();
+
+  console.log(
+    "%cAhh my guy just stop, it's pointless",
+    "color: red; font-weight: bold; font-size: 20px;"
+  );
 
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme") || "light";
@@ -66,24 +60,47 @@ function App() {
               />
               <Route path="/home" element={<Home />} index />
 
-              <Route path="/aboutus" element={<AboutUs />} />
-              <Route path="/learners" element={<Learners />} />
-              <Route path="/contactus" element={<Home id="contact-us" />} />
+              {/* About Us Routes */}
+              <Route path="/whoweare" element={<AboutUs />} />
+              <Route path="/ourMandate" element={<AboutUs />} />
+              <Route path="/accountingAuthority" element={<AboutUs />} />
+              <Route path="/management" element={<AboutUs />} />
+              <Route path="/constitution" element={<AboutUs />} />
+              <Route path="/policies" element={<AboutUs />} />
+
+              {/* Learning Programme Routes */}
+              <Route path="/learnerOverview" element={<Learners />} />
+              <Route path="/bursaries" element={<Learners />} />
+              <Route path="/leanerships" element={<Learners />} />
+              <Route path="/internships" element={<Learners />} />
+              <Route path="/skillsProgrammes" element={<Learners />} />
 
               {/* Employer Routes */}
-              <Route path="/mandateGrants" element={<Employers />}/>
+              <Route path="/mandateGrants" element={<Employers />} />
               <Route path="/discreteGrants" element={<Employers />} />
-              <Route path="/learningEvents" element={<Employers />}/>
+              <Route path="/learningEvents" element={<Employers />} />
 
               {/* Quality Assurance Routes */}
               <Route path="/overview" element={<ProfessionalBodies />} />
-              <Route path="/SDPAccreditation" element={<ProfessionalBodies />}/>
+              <Route
+                path="/SDPAccreditation"
+                element={<ProfessionalBodies />}
+              />
               <Route path="/accreditation" element={<ProfessionalBodies />} />
-              <Route path="/assessmentCentresAccreditation" element={<ProfessionalBodies />}/>
+              <Route
+                path="/assessmentCentresAccreditation"
+                element={<ProfessionalBodies />}
+              />
               <Route path="/SMERegistration" element={<ProfessionalBodies />} />
               <Route path="/EISA" element={<ProfessionalBodies />} />
-              <Route path="/workplaceApproval" element={<ProfessionalBodies />}/>
-              <Route path="/externalModerationProcess" element={<ProfessionalBodies />} />
+              <Route
+                path="/workplaceApproval"
+                element={<ProfessionalBodies />}
+              />
+              <Route
+                path="/externalModerationProcess"
+                element={<ProfessionalBodies />}
+              />
               <Route path="/QCTOPolicies" element={<ProfessionalBodies />} />
 
               {/* Tenders Routes */}
@@ -101,31 +118,6 @@ function App() {
               <Route path="/openPositions" element={<Vacancies />} />
               <Route path="/pastPositions" element={<Vacancies />} />
               <Route path="/vacancies/:id" element={<VacancyDetails />} />
-              {/* Mobile Routes */}
-
-              {/* About Us Routes */}
-              <Route path="/whoweare" element={<WhoWeAreMobile />} />
-              <Route path="/ourMandate" element={<OurMandateMobile />} />
-              <Route path="/board" element={<BoardMobile />} />
-              <Route path="/committees" element={<CommitteesMobile />} />
-              <Route path="/constitution" element={<ConstitutionMobile />} />
-              <Route path="/policies" element={<PoliciesMobile />} />
-
-              {/* Learning Programme Routes */}
-              <Route path="/learnerOverview" element={<ChooseCareerMobile />} />
-              <Route path="/bursaries" element={<BursaryMobile />} />
-              <Route
-                path="/leanerships"
-                element={<AboutFassetLearnershipsMobile />}
-              />
-              <Route path="/internships" element={<InternshipsMobile />} />
-              {/* <Route
-                path="/skillsProgrammes"
-                element={<SkillsProgrammeMobile />}
-              /> */}
-          
-
-              
             </Route>
           </Routes>
         </Router>

1313f0fe23393d213bf6c6ebfb227ee446633fe4
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-11 12:39:35 +0200

    Update Policies.jsx

--- src/components/AboutUs/Policies.jsx
+++ src/components/AboutUs/Policies.jsx
@@ -17,7 +17,7 @@ const Policies = () => {
       </Typography>
       <Divider />
       <Typography textAlign="justify">
-        Fasset’s Language Policy Fasset’s Language Policy adheres to the
+        FASSET's Language Policy FASSET’s Language Policy adheres to the
         requirement of the Use of Official Languages Act, No. 12 of 2012 and is
         aligned to the principle of promoting functional multilingualism as
         enshrined in the Constitution of the Republic of South Africa, 1996 and
@@ -30,7 +30,7 @@ const Policies = () => {
         >
           click here.
         </a>{" "}
-        Fasset has in place a Proatia Manual in terms of Section 14 of the
+        FASSET has in place a Proatia Manual in terms of Section 14 of the
         Promotion of Access to Information Act, 2000 (Act No. 2 of 2000) ("the
         Act"). The Act gives effect to the provisions of Section 32 of the
         Constitution which provides for the right of access to information held
@@ -83,21 +83,21 @@ const Policies = () => {
       <Divider />
       <Typography fontWeight="bold">Fraud Policy Statement</Typography>
       <Typography textAlign="justify">
-        Fasset’s Fraud Prevention Plan is implemented as a means of
+        FASSET’s Fraud Prevention Plan is implemented as a means of
         understanding and managing our fraud risks. A key element of this plan
-        is Fasset's declaration of zero tolerance towards crime. The Fraud
-        Policy Statement documented below states Fasset's intentions in this
+        is FASSET's declaration of zero tolerance towards crime. The Fraud
+        Policy Statement documented below states FASSET's intentions in this
         regard clearly: Fraud Policy Statement The Financial and Accounting
-        Services Sector Education and Training Authority (Fasset) is committed
+        Services Sector Education and Training Authority (FASSET) is committed
         to protecting its revenue, expenditure, assets and its reputation from
         any attempt by any person to gain financial or other benefit in an
         unlawful, dishonest or unethical manner. Coupled with this Statement,
-        Fasset has implemented a Charter for the implementation of the Fraud
+        FASSET has implemented a Charter for the implementation of the Fraud
         Prevention Plan and to give effect to the Fraud Policy Statement. This
         Charter is documented below: Anti-Fraud Charter In implementing the
-        Fraud Policy of Fasset: We are maintaining an anti-fraud environment
-        throughout Fasset The fraud prevention plan is being implemented
-        throughout Fasset. Fraud reduction is a priority in all governing
+        Fraud Policy of FASSET: We are maintaining an anti-fraud environment
+        throughout FASSET The fraud prevention plan is being implemented
+        throughout FASSEt. Fraud reduction is a priority in all governing
         structures (e.g. the Management Board, the various working committees,
         and the Audit Committee). We will continue to embrace and acknowledge
         the contribution of all employees and members of the community who
@@ -111,24 +111,24 @@ const Policies = () => {
         and indirect losses incurred via fraud, through effective information
         and communication. We react swiftly when a crime is uncovered We react
         swiftly and appropriately when a crime is uncovered. In order to assist
-        Fasset in giving effect to our Fraud Prevention Plan, we encourage all
-        stakeholders to make use of the Fasset Tip Offs Hotline (0800 205 054)
-        to notify Fasset of any suspected fraudulent activities impacting on
-        Fasset.
+        FASSET in giving effect to our Fraud Prevention Plan, we encourage all
+        stakeholders to make use of the FASSET Tip Offs Hotline (0800 205 054)
+        to notify FASSET of any suspected fraudulent activities impacting on
+        FASSET.
       </Typography>
       <Divider />
       <Typography fontWeight="bold">Publication Distribution</Typography>
       <Typography textAlign="justify">
-        Fasset would like to advise all publishers of publications which are not
+        FASSET would like to advise all publishers of publications which are not
         official Government issues that we are not able to accept for
         distribution to our membership any deliveries of stocks of newpapers,
         newsletters, journals or magazines. Any such unsolicited deliveries made
-        to the Fasset offices will not be accepted.
+        to the FASSET offices will not be accepted.
       </Typography>
       <Typography textAlign="justify">
         There may be exceptional circumstances where a specific edition of a
-        specific publication will be accepted by Fasset, but prior arrangements
-        are to be made with the Fasset manager responsible for the subsequent
+        specific publication will be accepted by FASSET, but prior arrangements
+        are to be made with the FASSET manager responsible for the subsequent
         distribution and delivery of stocks is to be addressed to the specified
         manager and accepted by them.
       </Typography>
